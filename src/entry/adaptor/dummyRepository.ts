ba221feef4f26ae248445c8b1eefba7f5e3a655b
Tatsuto YAMAMOTO <me@laminne33569.net>
2023-12-03 20:28:36 +0900

    feat(service): EntryServiceの実装

--- src/entry/adaptor/dummyRepository.ts
+++ src/entry/adaptor/dummyRepository.ts
@@ -1,5 +1,5 @@
 import {EntryRepository} from "../repository.js";
-import {Result} from "@mikuroxina/mini-fn";
+import {Option, Result} from "@mikuroxina/mini-fn";
 import {Entry} from "../entry.js";
 
 export class DummyRepository implements EntryRepository{
@@ -11,5 +11,17 @@ export class DummyRepository implements EntryRepository{
     async create(entry: Entry): Promise<Result.Result<Error, Entry>> {
         this.data.push(entry);
         return Result.ok(entry);
+    }
+
+    async findByTeamName(name: string): Promise<Option.Option<Entry>> {
+        const entry = this.data.find(e => e.teamName === name);
+        if (entry === undefined) {
+            return Option.none();
+        }
+        return Option.some(entry);
+    }
+
+    reset() {
+        this.data = [];
     }
 }
--- src/entry/service/entryService.test.ts
+++ src/entry/service/entryService.test.ts
@@ -1,4 +1,4 @@
-import {describe, expect, it} from "vitest";
+import {afterEach, describe, expect, it} from "vitest";
 import {DummyRepository} from "../adaptor/dummyRepository.js";
 import {EntryService} from "./entryService.js";
 import {Entry} from "../entry.js";
@@ -8,18 +8,85 @@ describe("entryService", () => {
     const repository = new DummyRepository();
     const service = new EntryService(repository);
 
-    it("エントリーできる",async () => {
+    afterEach(() => {
+        repository.reset();
+    })
+
+    it("エントリーできる", async () => {
         const entry = Entry.new({
             id: "123",
             teamName: "team1",
-            members: ["山田四十郎","石田四成"],
+            members: ["山田四十郎"],
             isMultiWalk: true,
             category: "Open"
         });
         const actual = await service.create(entry);
 
         expect(Result.isOk(actual)).toBe(true);
-        expect(actual[1]).toBe(entry)
-
+        expect(actual[1]).toBe(entry);
     });
+
+    it("チーム名が重複するときはエラー終了する", async () => {
+        const entry1 = Entry.new({
+            id: "123",
+            teamName: "team1",
+            members: ["山田四十郎"],
+            isMultiWalk: true,
+            category: "Open"
+        });
+        const entry2 = Entry.new({
+            id: "123",
+            teamName: "team1",
+            members: ["山田四十郎"],
+            isMultiWalk: true,
+            category: "Open"
+        });
+        await service.create(entry1);
+        const result = await service.create(entry2);
+
+        expect(Result.isErr(result)).toBe(true);
+        expect(result[1]).toStrictEqual(new Error("teamName Exists"));
+    });
+
+    it("オープン部門のメンバーは1人のみ", async () => {
+        const entry = Entry.new({
+            id: "123",
+            teamName: "team1",
+            members: ["山田四十郎", "山田太郎"],
+            isMultiWalk: true,
+            category: "Open"
+        });
+        const actual = await service.create(entry);
+
+        expect(Result.isErr(actual)).toBe(true);
+        expect(actual[1]).toStrictEqual(new Error("too many members"));
+    });
+
+    it("小学生部門のメンバーは1または2人", async () => {
+        const entry = Entry.new({
+            id: "123",
+            teamName: "team1",
+            members: ["山田四十郎", "山田太郎", "山田次郎"],
+            isMultiWalk: true,
+            category: "Elementary"
+        });
+        const actual = await service.create(entry);
+
+        expect(Result.isErr(actual)).toBe(true);
+        expect(actual[1]).toStrictEqual(new Error("too many members"));
+    });
+
+    it("メンバーが居ないチームは作れない", async () => {
+        const entry = Entry.new({
+            id: "123",
+            teamName: "team1",
+            members: [],
+            isMultiWalk: true,
+            category: "Elementary"
+        });
+        const actual = await service.create(entry);
+
+        expect(Result.isErr(actual)).toBe(true);
+        expect(actual[1]).toStrictEqual(new Error("no member"));
+    })
 })
