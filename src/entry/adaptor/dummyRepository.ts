2826952b325c8c4d247d03e3744220d29d3a5efb
Tatsuto YAMAMOTO <me@laminne33569.net>
2023-12-03 20:43:14 +0900

    feat(test): テストの仮実装

--- src/entry/adaptor/dummyRepository.ts
+++ src/entry/adaptor/dummyRepository.ts
@@ -1,27 +1,27 @@
-import {EntryRepository} from "../repository.js";
-import {Option, Result} from "@mikuroxina/mini-fn";
-import {Entry} from "../entry.js";
+import { EntryRepository } from "../repository.js";
+import { Option, Result } from "@mikuroxina/mini-fn";
+import { Entry } from "../entry.js";
 
-export class DummyRepository implements EntryRepository{
-    private data: Array<Entry>
-    constructor() {
-        this.data = [];
-    }
+export class DummyRepository implements EntryRepository {
+  private data: Array<Entry>;
+  constructor() {
+    this.data = [];
+  }
 
-    async create(entry: Entry): Promise<Result.Result<Error, Entry>> {
-        this.data.push(entry);
-        return Result.ok(entry);
-    }
+  async create(entry: Entry): Promise<Result.Result<Error, Entry>> {
+    this.data.push(entry);
+    return Result.ok(entry);
+  }
 
-    async findByTeamName(name: string): Promise<Option.Option<Entry>> {
-        const entry = this.data.find(e => e.teamName === name);
-        if (entry === undefined) {
-            return Option.none();
-        }
-        return Option.some(entry);
+  async findByTeamName(name: string): Promise<Option.Option<Entry>> {
+    const entry = this.data.find((e) => e.teamName === name);
+    if (entry === undefined) {
+      return Option.none();
     }
+    return Option.some(entry);
+  }
 
-    reset() {
-        this.data = [];
-    }
+  reset() {
+    this.data = [];
+  }
 }
---
+++ src/entry/service/get.test.ts
@@ -0,0 +1,11 @@
+import { describe, it } from "vitest";
+
+describe("getEntryService", () => {
+  it("すべて取得できる", () => {});
+
+  it("チーム名で取得できる", () => {});
+
+  it("チームIDで取得できる", () => {});
+
+  it("存在しないときはエラーを返す", () => {});
+});
