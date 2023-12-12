ab147deaa42585c41dcb298b27e4ae4f7beca0cf
kiharu3112 <kiharu3112@gmail.com>
2023-12-13 08:27:21 +0900

    prettierとeslintを導入した

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import { RouterProvider } from "react-router-dom";
 
 import { router } from "./routes/router.tsx";
 import "./App.css";
-import "@mantine/core/styles.css"
+import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 
 const App = () => {
-  return(
+  return (
     <MantineProvider>
       <RouterProvider router={router} />
     </MantineProvider>
