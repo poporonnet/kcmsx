3cfb5f14590f1866003398a703ae7eb23ba2aa30
tufusa <tufusa74@gmail.com>
2023-12-13 14:39:55 +0900

    chore: リファクタリング

--- src/pages/entryList.tsx
+++ src/pages/entryList.tsx
@@ -1,6 +1,9 @@
-import { Space, Table, Title } from "@mantine/core";
+import { Flex, Table, Title } from "@mantine/core";
 import "./entryList.css";
-const elements = [
+
+type Team = { name: string; member: string[]; isWalk: boolean };
+
+const teams: Team[] = [
   { name: "特攻野郎Aチーム", member: ["Canada", "America"], isWalk: true },
   { name: "全力投球", member: ["マララ", "よしき"], isWalk: false },
   { name: "ハイパーチーム", member: ["ハイパー", "チーム"], isWalk: true },
@@ -8,39 +11,36 @@ const elements = [
   { name: "ももクロ", member: ["ももクロ", "人生"], isWalk: false },
   { name: "アベノミクス", member: ["アベノミクス", "ふみお"], isWalk: true },
 ];
-export const EntryList = () => {
-  const rows = elements.map((element) => (
-    <Table.Tr key={element.name}>
-      <Table.Td className={"td"}>{element.name}</Table.Td>
-      <Table.Td className={"td"}>{element.member.join(",")}</Table.Td>
-      <Table.Td className={"td"}>{element.isWalk ? "歩行" : "車輪"}</Table.Td>
-    </Table.Tr>
-  ));
-  return (
-    <div>
-      <Title order={3}>小中学生部門</Title>
-      <Table striped withTableBorder>
-        <Table.Thead>
-          <Table.Tr>
-            <Table.Th>チーム名</Table.Th>
-            <Table.Th>メンバー</Table.Th>
-            <Table.Th>タイプ</Table.Th>
+
+export const EntryList = () => (
+  <Flex direction="column" gap={20}>
+    <EntryTable categoryName="小中学生部門" teams={teams} />
+    <EntryTable categoryName="オープン部門" teams={teams} />
+  </Flex>
+);
+
+const EntryTable = (props: { categoryName: string; teams: Team[] }) => (
+  <div>
+    <Title order={3}>{props.categoryName}</Title>
+    <Table striped withTableBorder miw="40rem">
+      <Table.Thead>
+        <Table.Tr>
+          <Table.Th>チーム名</Table.Th>
+          <Table.Th>メンバー</Table.Th>
+          <Table.Th>タイプ</Table.Th>
+        </Table.Tr>
+      </Table.Thead>
+      <Table.Tbody>
+        {props.teams.map((element) => (
+          <Table.Tr key={element.name}>
+            <Table.Td className="td">{element.name}</Table.Td>
+            <Table.Td className="td">{element.member.join(",")}</Table.Td>
+            <Table.Td className="td">
+              {element.isWalk ? "歩行" : "車輪"}
+            </Table.Td>
           </Table.Tr>
-        </Table.Thead>
-        <Table.Tbody>{rows}</Table.Tbody>
-      </Table>
-      <Space h={20} />
-      <Title order={3}>オープン部門</Title>
-      <Table striped withTableBorder>
-        <Table.Thead>
-          <Table.Tr>
-            <Table.Th>チーム名</Table.Th>
-            <Table.Th>メンバー</Table.Th>
-            <Table.Th>タイプ</Table.Th>
-          </Table.Tr>
-        </Table.Thead>
-        <Table.Tbody>{rows}</Table.Tbody>
-      </Table>
-    </div>
-  );
-};
+        ))}
+      </Table.Tbody>
+    </Table>
+  </div>
+);
