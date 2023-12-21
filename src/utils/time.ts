694f1990c9a2bb9a5cc6373493fbf7c7bdd0baf5
tufusa <tufusa74@gmail.com>
2023-12-21 18:08:47 +0900

    feat: 秒をフォーマットする関数

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -3,3 +3,14 @@ export const expiryTimestamp = (expirySeconds: number) => {
   time.setSeconds(time.getSeconds() + expirySeconds);
   return time;
 };
+
+export const parseSeconds = (seconds: number): string => {
+  const time = {
+    minutes: Math.floor(seconds / 60),
+    seconds: seconds % 60,
+  };
+
+  const justify = (value: number) => value.toString().padStart(2, "0");
+
+  return `${justify(time.minutes)}:${justify(time.seconds)}`;
+};
