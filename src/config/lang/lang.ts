9b8be6b921360d2d7fee04ff53198c56ffe9fb43
tufusa <tufusa74@gmail.com>
2023-12-25 14:34:13 +0900

    chore: 試合ページの文字列を修正

--- src/config/lang/lang.ts
+++ src/config/lang/lang.ts
@@ -3,7 +3,7 @@ type Lang = Record<string, string | Record<string, string>>;
 export const lang = {
   match: {
     leaveBase: "松江エリアを出た",
-    overMiddle: "中間線を越えた",
+    overMiddle: "中間点を越えた",
     enterDistination: "金星エリアに入った",
     placeBall: "ボールを金星エリアに置いた",
     returnBase: "松江エリアに戻った",
