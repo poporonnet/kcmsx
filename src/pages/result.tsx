f88509fec0dca5d35d3be62ff602b617d0a41ef4
Suzune2741 <kannra1174@gmail.com>
2023-12-26 18:15:11 +0900

    feat: 試合結果一覧のページ

--- src/pages/result.tsx
+++ src/pages/result.tsx
@@ -1,133 +1,68 @@
 import { Flex, Table, Title } from "@mantine/core";
 import React, { useState, useEffect } from "react";
 import "./result.css";
+
 type MatchTeam = {
-  id: string;
-  teamName: string;
-  isMultiWalk: boolean;
-  category: string;
+  left: {
+    id: string;
+    teamName: string;
+    isMultiWalk: boolean;
+    category: string;
+  };
+  right: {
+    id: string;
+    teamName: string;
+    isMultiWalk: boolean;
+    category: string;
+  };
+};
+type Result = {
+  left: {
+    teamID: string;
+    points: number;
+    time: number;
+  };
+  right: {
+    teamID: string;
+    points: number;
+    time: number;
+  };
 };
 type Match = {
   id: string;
-  teams: MatchTeam[];
+  teams: MatchTeam;
   matchType: string;
-  points: number[];
   courseIndex: number;
-  time: number[];
-  winnerID: string;
+  results: Result;
 };
 
 export const Result = () => {
-  const [match, setMatch] = useState<Data[]>([]);
+  const [primarymatch, setprimaryMatch] = useState<Match[]>([]);
+  const [finalmatch, setfinalMatch] = useState<Match[]>([]);
   useEffect(() => {
-    {
-      /*fetch("http://localhost:3000/match/primary")*/
-    }
-    fetch("http://localhost:3000/entry")
+    fetch("http://localhost:3000/match/primary")
       .then((res) => res.json())
-      .then((json) => setMatch(json))
+      .then((json) => setprimaryMatch(json))
       .catch(() => alert("error"));
   }, []);
+  // useEffect(() => {
+  //   fetch("http://localhost:3000/match/final")
+  //     .then((res) => res.json())
+  //     .then((json) => setfinalMatch(json))
+  //     .catch(() => alert("error"));
+  // }, []);
 
-  console.log(match);
   return (
     <>
       <Flex direction="column" gap={20}>
-        <FinalTable categoryName="本選" teams={teams} matches={matches} />
-        <PrimaryTable categoryName="予選" matches={matches} />
+        <FinalTable categoryName="本選" matches={primarymatch} />
+        <PrimaryTable categoryName="予選" matches={primarymatch} />
       </Flex>
     </>
   );
 };
-type Data = {
-  id: string;
-  teamName: string;
-  members: string[];
-  isMultiWalk: boolean;
-  category: string;
-};
-type Team = {
-  id: string;
-  teamName: string;
-  member: string[];
-  isMultiWalk: boolean;
-  category: string;
-};
 
-const teams: Team[] = [
-  {
-    id: "1",
-    teamName: "特攻野郎Aチーム",
-    member: ["Canada", "America"],
-    isMultiWalk: true,
-    category: "Primary",
-  },
-  {
-    id: "2",
-    teamName: "全力投球",
-    member: ["マララ", "よしき"],
-    isMultiWalk: false,
-    category: "Primary",
-  },
-  {
-    id: "3",
-    teamName: "ハイパーチーム",
-    member: ["ハイパー", "チーム"],
-    isMultiWalk: true,
-    category: "Primary",
-  },
-  {
-    id: "4",
-    teamName: "レンちゃん",
-    member: ["レンちゃん", "リンちゃん"],
-    isMultiWalk: true,
-    category: "Primary",
-  },
-  {
-    id: "5",
-    teamName: "ももクロ",
-    member: ["ももクロ", "人生"],
-    isMultiWalk: false,
-    category: "Primary",
-  },
-  {
-    id: "6",
-    teamName: "アベノミクス",
-    member: ["アベノミクス", "ふみお"],
-    isMultiWalk: true,
-    category: "Primary",
-  },
-];
-const matches: Match[] = [
-  {
-    id: "39440930485092",
-    teams: [
-      {
-        id: "39440930485098",
-        teamName: "千葉ロッテ",
-        isMultiWalk: false,
-        category: "Primary",
-      },
-      {
-        id: "93454093",
-        teamName: "阪神タイガース",
-        isMultiWalk: false,
-        category: "Primary",
-      },
-    ],
-    matchType: "Primary",
-    points: [5, 3],
-    courseIndex: 1,
-    time: [100, 120],
-    winnerID: "39440930485098",
-  },
-];
-
-const FinalTable = (props: {
-  categoryName: string;
-  teams: Team[];
-  matches: Match[];
-}) => (
+const FinalTable = (props: { categoryName: string; matches: Match[] }) => (
   <div>
     {}
     <Title order={3}>{props.categoryName}</Title>
@@ -142,9 +77,31 @@ const FinalTable = (props: {
       <Table.Tbody>
         {props.matches.map((element) => (
           <Table.Tr key={element.id[0]}>
-            <Table.Td className="td">{element.teams[0].teamName}</Table.Td>
-            <Table.Td className="td">{element.points.join("-")}</Table.Td>
-            <Table.Td className="td">{element.teams[1].teamName}</Table.Td>
+            {element.results?.left.points > element.results?.right.points ? (
+              <>
+                <Table.Td className="td">
+                  {element.teams.left.teamName}
+                </Table.Td>
+                <Table.Td className="td">
+                  {element.results?.right.points}-{element.results?.left.points}
+                </Table.Td>
+                <Table.Td className="td">
+                  {element.teams.right.teamName}
+                </Table.Td>
+              </>
+            ) : (
+              <>
+                <Table.Td className="td">
+                  {element.teams.right.teamName}
+                </Table.Td>
+                <Table.Td className="td">
+                  {element.results?.right.points}-{element.results?.left.points}
+                </Table.Td>
+                <Table.Td className="td">
+                  {element.teams.left.teamName}
+                </Table.Td>
+              </>
+            )}
           </Table.Tr>
         ))}
       </Table.Tbody>
@@ -170,12 +127,16 @@ const PrimaryTable = (props: { categoryName: string; matches: Match[] }) => (
         <Table.Tbody>
           {props.matches.map((element) => (
             <Table.Tr key={element.id[0]}>
-              <Table.Td className="td">{element.teams[0].teamName}</Table.Td>
-              <Table.Td className="td">{element.time[0]}</Table.Td>
-              <Table.Td className="td">{element.points[0]}</Table.Td>
-              <Table.Td className="td">{element.teams[1].teamName}</Table.Td>
-              <Table.Td className="td">{element.time[1]}</Table.Td>
-              <Table.Td className="td">{element.points[1]}</Table.Td>
+              <Table.Td className="td">{element.teams?.left.teamName}</Table.Td>
+              <Table.Td className="td">{element.results?.left.time}</Table.Td>
+              <Table.Td className="td">{element.results?.left.points}</Table.Td>
+              <Table.Td className="td">
+                {element.teams?.right.teamName}
+              </Table.Td>
+              <Table.Td className="td">{element.results?.right.time}</Table.Td>
+              <Table.Td className="td">
+                {element.results?.right.points}
+              </Table.Td>
             </Table.Tr>
           ))}
         </Table.Tbody>
