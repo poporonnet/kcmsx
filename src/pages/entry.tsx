4a98bf3826b260cb2327205af99ac161dd9452be
kiharu3112 <kiharu3112@gmail.com>
2023-12-29 03:27:41 +0900

    chore:fetch対応した

--- src/pages/entry.tsx
+++ src/pages/entry.tsx
@@ -1,12 +1,12 @@
 import { TextInput, Box, SegmentedControl, Button, Group } from "@mantine/core";
 import { useState } from "react";
 export const Entry = () => {
-  const url = "http://localhost:3000";
   const [teamName, setTeamName] = useState("");
   const [members, setMembers] = useState(["", ""]);
   const [isMultiWalk, setIsMultiWalk] = useState(true); // ロボットが多足歩行型か
   const [category, setCategory] = useState("Elementary"); // 出場する部門 小学生部門:メンバー最大2人 オープン部門：1人
-  async function submit() {
+  async function submit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
     // メンバーは、オープン部門または小学生部門かつメンバーが1人の場合は配列の要素数を1つにする
     const data = {
       teamName: teamName,
@@ -19,76 +19,78 @@ export const Entry = () => {
       isMultiWalk: isMultiWalk,
       category: category,
     };
-    console.log("url is ", url + "/entry");
-    const res = await fetch(url + "/entry", {
+    const res = await fetch(`${import.meta.env.VITE_API_URL}/entry`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
     });
-    const json = await res.json();
-    console.log(json);
+    if (res.ok) {
+      alert("登録しました");
+    } else {
+      alert("登録に失敗しました");
+    }
   }
   return (
-    <Box maw={340} mx={"auto"}>
-      <TextInput
-        mt={"md"}
-        label="チーム名"
-        required
-        placeholder="チーム名を入力してください"
-        value={teamName}
-        onChange={(event) => setTeamName(event.currentTarget.value)}
-      />
-      <SegmentedControl
-        mt={"md"}
-        fullWidth
-        data={[
-          { label: "小学生部門", value: "Elementary" },
-          { label: "オープン部門", value: "Open" },
-        ]}
-        value={category}
-        onChange={(value) => setCategory(value)}
-      />
-      <TextInput
-        mt={"md"}
-        label="メンバー"
-        required
-        placeholder="メンバーを入力してください"
-        value={members[0]}
-        onChange={(event) =>
-          setMembers([event.currentTarget.value, members[1]])
-        }
-      />
-      <TextInput
-        mt={"md"}
-        placeholder="メンバーを入力してください"
-        value={members[1]}
-        disabled={!(category === "Elementary")}
-        onChange={(event) =>
-          setMembers([members[0], event.currentTarget.value])
-        }
-      />
-      <SegmentedControl
-        fullWidth
-        mt={"md"}
-        data={[
-          { label: "歩行型", value: "walk" },
-          { label: "車輪型", value: "tire" },
-        ]}
-        value={isMultiWalk ? "walk" : "tire"}
-        onChange={(value) => setIsMultiWalk(value === "walk")}
-      />
-      <Group justify={"flex-start"}>
-        <Button
+    <Box maw={620} mx={"auto"}>
+      <form onSubmit={submit}>
+        <TextInput
           mt={"md"}
-          onClick={() => {
-            submit();
+          label="チーム名"
+          required
+          placeholder="チーム名を入力してください"
+          value={teamName}
+          onChange={(event) => setTeamName(event.currentTarget.value)}
+        />
+        <SegmentedControl
+          mt={"md"}
+          fullWidth
+          data={[
+            { label: "小学生部門", value: "Elementary" },
+            { label: "オープン部門", value: "Open" },
+          ]}
+          value={category}
+          onChange={(value) => setCategory(value)}
+        />
+        <TextInput
+          mt={"md"}
+          label="メンバー"
+          required
+          placeholder="メンバーを入力してください"
+          value={members[0]}
+          onChange={(event) => {
+            setMembers([event.currentTarget.value, members[1]]);
           }}
-        >
-          登録
-        </Button>
-      </Group>
+        />
+        <TextInput
+          mt={"md"}
+          placeholder="メンバーを入力してください"
+          value={members[1]}
+          onChange={(event) => {
+            setMembers([members[0], event.currentTarget.value]);
+          }}
+          disabled={!(category === "Elementary")}
+        />
+        <SegmentedControl
+          fullWidth
+          mt={"md"}
+          data={[
+            { label: "歩行型", value: "walk" },
+            { label: "車輪型", value: "tire" },
+          ]}
+          value={isMultiWalk ? "walk" : "tire"}
+          onChange={(value) => setIsMultiWalk(value === "walk")}
+        />
+        <Group justify={"flex-start"}>
+          <Button
+            mt={"md"}
+            type="submit"
+          >
+            登録
+          </Button>
+        </Group>
+      </form>
     </Box>
   );
 };
