7da69df260d01b0c2ea1a418a95d72d08832b02b
speak-mentaiko <1717miyawaki@gmail.com>
2023-12-30 23:51:16 +0900

    feat:ランキングを表示

--- src/pages/ranking.tsx
+++ src/pages/ranking.tsx
@@ -9,27 +9,10 @@ type Ranking = {
   time: number;
 };
 
-const teams: Ranking[] = [
-  { name: "特攻野郎Aチーム", id: "1", point: 4, time: 300 },
-  { name: "全力投球", id: "2", point: 5, time: 260.0 },
-  { name: "ハイパーチーム", id: "3", point: 2, time: 310.0 },
-  { name: "レンちゃん", id: "4", point: 1, time: 350.0 },
-  { name: "ももクロ", id: "5", point: 7, time: 290.0 },
-  { name: "バカ", id: "6", point: 4, time: 320.0 },
-  { name: "test", id: "7", point: 1, time: 900 },
-];
-
-const ranking = teams.sort(function (a, b) {
-  if (a.point == b.point) {
-    return a.time > b.time ? 1 : -1;
-  }
-  return a.point > b.point ? -1 : 1;
-});
-
 let rankdata: Ranking[] = [];
 
 const FetchData = () => {
-  let rankdata: Ranking[] = [];
+  rankdata = [];
   const [resultdata, setData] = useState();
   useEffect(() => {
     fetch("http://localhost:3000/match/primary", { method: "GET" })
@@ -40,33 +23,26 @@ const FetchData = () => {
 
   if (resultdata != undefined) {
     resultdata.map((game) => {
-      if (`results` in game) {
+      if (game.results && game.matchType == "primary") {
         Object.keys(game.results).map((elm) => {
           const tmp = { name: "str", id: "0", point: 0, time: 0 };
-          if (elm == "left") {
-            tmp.name = game.teams.left.teamName;
-            tmp.id = game.results.left.teamID;
-            tmp.point = game.results.left.points;
-            tmp.time = game.results.left.time;
-            rankdata.push(tmp);
-          } else if (elm == "right") {
-            tmp.name = game.teams.right.teamName;
-            tmp.id = game.results.right.teamID;
-            tmp.point = game.results.right.points;
-            tmp.time = game.results.right.time;
-            rankdata.push(tmp);
-          }
+          tmp.name = game.teams[elm].teamName;
+          tmp.id = game.results[elm].teamID;
+          tmp.point = game.results[elm].points;
+          tmp.time = game.results[elm].time;
+          rankdata.push(tmp);
         });
       }
     });
   }
 
-  console.log("rank", rankdata);
-  return (
-    <>
-      <div>test</div>
-    </>
-  );
+  rankdata = rankdata.sort(function (a, b) {
+    if (a.point == b.point) {
+      return a.time > b.time ? 1 : -1;
+    }
+    return a.point > b.point ? -1 : 1;
+  });
+  return;
 };
 
 export const Ranking = () => (
@@ -74,11 +50,11 @@ export const Ranking = () => (
     <FetchData />
     <h1>ランキング</h1>
     <h2>小学生部門</h2>
-    <RankingTable categoryName="予選" teams={rankdata} />
+    <RankingTable categoryName="予選" ranking={rankdata} />
   </Flex>
 );
 
-const RankingTable = (props: { categoryName: string; teams: Ranking[] }) => (
+const RankingTable = (props: { categoryName: string; ranking: Ranking[] }) => (
   <div>
     <Title order={3}>{props.categoryName}</Title>
     <Table striped withTableBorder miw="40rem">
@@ -91,7 +67,7 @@ const RankingTable = (props: { categoryName: string; teams: Ranking[] }) => (
         </Table.Tr>
       </Table.Thead>
       <Table.Tbody>
-        {props.teams.map((element, index) => (
+        {props.ranking.map((element, index) => (
           <Table.Tr key={element.name}>
             <Table.Td className="td">{index + 1}</Table.Td>
             <Table.Td className="td">{element.name}</Table.Td>
