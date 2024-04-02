52b128e066f898f72ee16de372b117440919ea29
Miharup <74781355+kiharu3112@users.noreply.github.com>
2024-04-02 13:01:24 +0100

    chore:テストの追加の変更 (#46)
    * feat:vitestのセットアップ
    
    * chore:packageManagerのshaハッシュを削除
    
    * chore:追加のテストの設定
    
    * feat:サンプルとしてheaderのちょっとしたテスト
    
    * chore:prettierをかけました
    
    * pnpm-lock.yamlをcommitするのを忘れてました
    
    ---------
    
    Co-authored-by: kijiharu <kijiharu>

---
+++ src/components/header.test.tsx
@@ -0,0 +1,29 @@
+import { AppShell } from "@mantine/core";
+import "@testing-library/jest-dom";
+import { screen } from "@testing-library/react";
+import { describe, expect, test } from "vitest";
+import { render } from "../../test";
+import { Header } from "./header.tsx";
+
+describe("header component", () => {
+  test("has /entrylist link", () => {
+    render(
+      <AppShell
+        header={{ height: 60, offset: true }}
+        footer={{ height: 30, offset: true }}
+        padding="md"
+      >
+        <Header />
+      </AppShell>
+    );
+
+    expect(screen.getByText("参加者一覧")).toHaveAttribute(
+      "href",
+      "/entrylist"
+    );
+    expect(screen.getByText("参加者登録")).toHaveAttribute("href", "/entry");
+    expect(screen.getByText("試合表")).toHaveAttribute("href", "/matchlist");
+    expect(screen.getByText("試合結果")).toHaveAttribute("href", "/result");
+    expect(screen.getByText("ランキング")).toHaveAttribute("href", "/ranking");
+  });
+});
