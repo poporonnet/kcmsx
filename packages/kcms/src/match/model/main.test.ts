8ac303db96bb080a93949d37f5fa75f9c8e407b6
Tatsuto YAMAMOTO <me@laminne33569.net>
2024-07-08 18:14:16 +0900

    fix: 本戦試合(`MainMatch`)を定義

---
+++ packages/kcms/src/match/model/main.test.ts
@@ -0,0 +1,176 @@
+import { describe, expect, it } from 'vitest';
+import { MainMatch, MainMatchID } from './main.js';
+import { EntryID } from '../../entry/entry.js';
+import { RunResult, RunResultID } from './runResult.js';
+
+describe('MainMatch', () => {
+  it('正しく初期化できる', () => {
+    const args = {
+      id: '1' as MainMatchID,
+      courseIndex: 1,
+      matchIndex: 1,
+      teamId1: '2' as EntryID,
+      teamId2: '3' as EntryID,
+      winnerId: '2' as EntryID,
+      runResults: [],
+    };
+
+    const res = MainMatch.new(args);
+    expect(res.getId()).toBe(args.id);
+    expect(res.getCourseIndex()).toBe(args.courseIndex);
+    expect(res.getMatchIndex()).toBe(args.matchIndex);
+    expect(res.getTeamId1()).toBe(args.teamId1);
+    expect(res.getTeamId2()).toBe(args.teamId2);
+    expect(res.getWinnerId()).toBe(args.winnerId);
+    expect(res.getRunResults()).toBe(args.runResults);
+  });
+
+  it('走行結果は0 or 2 or 4になる', () => {
+    const args = {
+      id: '1' as MainMatchID,
+      courseIndex: 1,
+      matchIndex: 1,
+      teamId1: '2' as EntryID,
+      teamId2: '3' as EntryID,
+      winnerId: '2' as EntryID,
+      runResults: [],
+    };
+
+    for (let i = 1; i < 100; i++) {
+      const mainMatch = MainMatch.new(args);
+      // 2か4以外は足せない
+      if (i == 2 || i == 4) {
+        expect(() => {
+          mainMatch.setRunResults(
+            [...Array(2)].map((_, i) => {
+              return RunResult.new({
+                id: String(i) as RunResultID,
+                goalTimeSeconds: i * 10,
+                points: 10 + i,
+                // ToDo: Team1かTeam2のどちらかを指定する
+                teamId: i % 2 == 0 ? args.teamId1 : args.teamId2,
+                finishState: 'FINISHED',
+              });
+            })
+          );
+        }).not.toThrow(new Error('RunResult length must be 2 or 4'));
+        continue;
+      }
+      expect(() => {
+        mainMatch.setRunResults(
+          [...Array(i)].map((_, i) => {
+            return RunResult.new({
+              id: String(i) as RunResultID,
+              goalTimeSeconds: i * 10,
+              points: 10 + i,
+              teamId: i % 2 == 0 ? args.teamId1 : args.teamId2,
+              finishState: 'FINISHED',
+            });
+          })
+        );
+      }).toThrow(new Error('RunResult length must be 2 or 4'));
+    }
+  });
+
+  it('勝者を指定できる', () => {
+    const args = {
+      id: '1' as MainMatchID,
+      courseIndex: 1,
+      matchIndex: 1,
+      teamId1: '2' as EntryID,
+      teamId2: '3' as EntryID,
+      runResults: [...Array(4)].map((_, i) =>
+        RunResult.new({
+          id: String(i) as RunResultID,
+          goalTimeSeconds: i * 10,
+          points: 10 + i,
+          teamId: i % 2 == 0 ? ('2' as EntryID) : ('3' as EntryID),
+          finishState: 'FINISHED',
+        })
+      ),
+    };
+
+    const mainMatch = MainMatch.new(args);
+
+    expect(mainMatch.setWinnerId('2' as EntryID)).toBe(undefined);
+  });
+
+  it('勝者が決まっているときは変更できない', () => {
+    const args = {
+      id: '1' as MainMatchID,
+      courseIndex: 1,
+      matchIndex: 1,
+      teamId1: '2' as EntryID,
+      teamId2: '3' as EntryID,
+      winnerId: '2' as EntryID,
+      runResults: [...Array(4)].map((_, i) => {
+        return RunResult.new({
+          id: String(i) as RunResultID,
+          goalTimeSeconds: i * 10,
+          points: 10 + i,
+          teamId: i % 2 == 0 ? ('2' as EntryID) : ('3' as EntryID),
+          finishState: 'FINISHED',
+        });
+      }),
+    };
+
+    const mainMatch = MainMatch.new(args);
+
+    expect(() => mainMatch.setWinnerId('2' as EntryID)).toThrowError(
+      new Error('WinnerId is already set')
+    );
+  });
+
+  it('試合が終わっていないときは設定できない', () => {
+    const args = {
+      id: '1' as MainMatchID,
+      courseIndex: 1,
+      matchIndex: 1,
+      teamId1: '2' as EntryID,
+      teamId2: '3' as EntryID,
+      runResults: [...Array(2)].map((_, i) => {
+        return RunResult.new({
+          id: String(i) as RunResultID,
+          goalTimeSeconds: i * 10,
+          points: 10 + i,
+          teamId: i % 2 == 0 ? ('2' as EntryID) : ('3' as EntryID),
+          finishState: 'FINISHED',
+        });
+      }),
+    };
+
+    const mainMatch = MainMatch.new(args);
+
+    expect(() => mainMatch.setWinnerId('2' as EntryID)).toThrowError(
+      new Error('This match is not finished')
+    );
+  });
+
+  it('勝者はteamId1かteamId2でなければならない', () => {
+    const args = {
+      id: '1' as MainMatchID,
+      courseIndex: 1,
+      matchIndex: 1,
+      teamId1: '2' as EntryID,
+      teamId2: '3' as EntryID,
+      runResults: [...Array(4)].map((_, i) => {
+        return RunResult.new({
+          id: String(i) as RunResultID,
+          goalTimeSeconds: i * 10,
+          points: 10 + i,
+          teamId: i % 2 == 0 ? ('2' as EntryID) : ('3' as EntryID),
+          finishState: 'FINISHED',
+        });
+      }),
+    };
+
+    const mainMatch = MainMatch.new(args);
+
+    expect(() => mainMatch.setWinnerId('0' as EntryID)).toThrowError(
+      new Error('WinnerId must be teamId1 or teamId2')
+    );
+    expect(() => mainMatch.setWinnerId('1' as EntryID)).toThrowError(
+      new Error('WinnerId must be teamId1 or teamId2')
+    );
+  });
+});
---
+++ packages/kcms/src/match/model/main.ts
@@ -0,0 +1,89 @@
+// 本戦試合
+
+import { SnowflakeID } from '../../id/main.js';
+import { RunResult } from './runResult.js';
+import { EntryID } from '../../entry/entry.js';
+
+export type MainMatchID = SnowflakeID<MainMatch>;
+export interface CreateMainMatchArgs {
+  id: MainMatchID;
+  courseIndex: number;
+  matchIndex: number;
+  teamId1?: EntryID;
+  teamId2?: EntryID;
+  winnerId?: EntryID;
+  runResults: RunResult[];
+}
+
+export class MainMatch {
+  private readonly id: MainMatchID;
+  private readonly courseIndex: number;
+  private readonly matchIndex: number;
+  private readonly teamId1?: EntryID;
+  private readonly teamId2?: EntryID;
+  private winnerId?: EntryID;
+  private runResults: RunResult[];
+
+  private constructor(args: CreateMainMatchArgs) {
+    this.id = args.id;
+    this.courseIndex = args.courseIndex;
+    this.matchIndex = args.matchIndex;
+    this.teamId1 = args.teamId1;
+    this.teamId2 = args.teamId2;
+    this.winnerId = args.winnerId;
+    this.runResults = args.runResults;
+  }
+
+  public static new(args: CreateMainMatchArgs) {
+    return new MainMatch(args);
+  }
+
+  getId(): MainMatchID {
+    return this.id;
+  }
+
+  getCourseIndex(): number {
+    return this.courseIndex;
+  }
+
+  getMatchIndex(): number {
+    return this.matchIndex;
+  }
+
+  getTeamId1(): string | undefined {
+    return this.teamId1;
+  }
+
+  getTeamId2(): string | undefined {
+    return this.teamId2;
+  }
+
+  getWinnerId(): string | undefined {
+    return this.winnerId;
+  }
+
+  setWinnerId(winnerId: EntryID) {
+    if (this.winnerId !== undefined) {
+      throw new Error('WinnerId is already set');
+    }
+    if (this.teamId1 !== winnerId && this.teamId2 !== winnerId) {
+      throw new Error('WinnerId must be teamId1 or teamId2');
+    }
+    if (this.runResults.length !== 4) {
+      throw new Error('This match is not finished');
+    }
+    this.winnerId = winnerId;
+  }
+
+  getRunResults(): RunResult[] {
+    return this.runResults;
+  }
+
+  setRunResults(results: RunResult[]) {
+    // 1チームが2つずつ結果を持つので、2 または 4個
+    if (results.length !== 4 && results.length !== 2) {
+      throw new Error('RunResult length must be 2 or 4');
+    }
+    this.runResults = results;
+  }
+}
