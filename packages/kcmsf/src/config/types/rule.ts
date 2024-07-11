4c9454171d836c40b66f42c4e335b90d4083c0fb
tufusa <tufusa74@gmail.com>
2024-07-11 16:26:51 +0900

    chore: コメントを追加

--- packages/kcmsf/src/config/types/rule.ts
+++ packages/kcmsf/src/config/types/rule.ts
@@ -3,22 +3,32 @@ import { Judge } from "../../utils/match/judge";
 import { ruleList } from "../rule/rule";
 
 /**
- * ルールの種別
+ * @description ルールの種別のユニオン
  * - `single`: 状態が真理値のみを持つもの e.g.`ゴールしたか否か`
  * - `count`: 状態が整数値を持つもの e.g.`持ち帰ったボールの個数`
  */
 type RuleType = "single" | "countable";
 
+/**
+ * @description {@link StateType}のキーを{@link RuleType}に制限するための型
+ */
 type _StateType<T extends Record<RuleType, any>> = T;
 
+/**
+ * @description {@link RuleType}を対応するルールの状態の型にマップする型
+ */
 type StateType = _StateType<{
   single: boolean;
   countable: number;
 }>;
 
+/**
+ * @description ありうるルールの状態すべてのユニオン cf. {@link StateType}
+ */
 export type StateTypes = StateType[RuleType];
 
 /**
+ * @description 1つのルールの型
  * @param name ルールのキー名 e.g.`leaveBase`
  * @param label ルールの表示名 e.g.`松江エリアを出た`
  * @param type ルールのタイプ cf.{@link RuleType}
@@ -42,46 +52,78 @@ type PointRule<
   visible?: (premiseState: PremiseState) => boolean;
 }>;
 
-type _RuleVariant<Type extends RuleType = RuleType> = Type extends Type
+/**
+ * @description 指定した{@link RuleType}に対応する{@link PointRule}のユニオン
+ */
+type _RuleVariant<Type extends RuleType> = Type extends Type
   ? PointRule<Type, StateType[Type]>
   : never;
 
-type RuleVariant = _RuleVariant;
+/**
+ * @description ありうる{@link PointRule}すべてのユニオン
+ */
+type RuleVariant = _RuleVariant<RuleType>;
 
+/**
+ * @description ルールの配列の型
+ */
 export type RuleList = RuleVariant[];
 
-
+/**
+ * @description 設定されたすべてのルールのユニオン
+ */
 export type Rule = (typeof ruleList)[number];
 
+/**
+ * @description 設定されたすべてのルールの`name`のユニオン
+ */
 export type RuleName = Rule["name"];
 
+/**
+ * @description ルールの初期状態の型
+ */
 export type InitialPointState = {
   [I in Rule as I["name"]]: I extends { name: I["name"] }
     ? I["initial"]
     : never;
 };
 
-type _PointState = {
+/**
+ * @description ルールの状態の型
+ */
+export type PointState = {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   [R in Rule as R["name"]]: R extends PointRule<infer Type, infer _Initial>
     ? StateType[Type]
     : never;
 };
 
-export type PointState = _PointState;
-
+/**
+ * @description ルールの初期状態のオブジェクト cf.{@link InitialPointState}
+ */
 export const initialPointState: InitialPointState = Object.fromEntries(
   ruleList.map((v) => [v.name, v.initial])
 ) as InitialPointState;
 
+/**
+ * @description 得点計算用の試合状態の型
+ */
 export type PremiseState = {
   matchInfo: MatchInfo | null;
   side: keyof MatchInfo["teams"];
   judge: Judge;
 };
 
+/**
+ * @description {@link RuleList}が有効か判定する型  
+ * {@link PointRule}の`name`属性が重複していたらコンパイルに失敗する
+ */
 export type ValidRuleList<R extends RuleList> = UniqueArray<R, "name">;
 
+/**
+ * @description リテラル配列`R`内のレコードの`Key`属性がユニークか検査する型  
+ * ユニークなら`R`自体, 重複していたらその旨のエラーメッセージの文字列リテラル型になる
+ */
 type UniqueArray<
   R extends Record<Key, string>[],
   Key extends keyof R[number],
@@ -93,6 +135,9 @@ type UniqueArray<
     : never
   : never;
 
+/**
+ * @description リテラル配列`R`内のレコードの`Key`属性を取り出した文字列リテラル配列の型
+ */
 type Pickup<
   R extends Record<Key, string>[],
   Key extends keyof R[number],
@@ -105,6 +150,10 @@ type Pickup<
     : never
   : A;
 
+/**
+ * @description 文字列リテラル配列`R`の要素がユニークか検査する型  
+ * ユニークなら`true`, 重複していたら`{ErrorMessage}: {重複している要素}`という文字列リテラル型になる
+ */
 type IsUnique<
   Array extends string[],
   ErrorMessage extends string,
