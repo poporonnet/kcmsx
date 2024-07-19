db9fe1c2d8378ecca7caccb31a6ce97c82871607
toranomonn <yuya8naito17@gmail.com>
2024-07-19 15:14:03 +0900

    chore: basenameの追加とコンフリクトの解決を行った

--- packages/kcmsf/src/routes/router.tsx
+++ packages/kcmsf/src/routes/router.tsx
@@ -8,39 +8,44 @@ import { Ranking } from "../pages/ranking.tsx";
 import { Result } from "../pages/result.tsx";
 import { Layout } from "./layout.tsx";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "entrylist",
+          element: <EntryList />,
+        },
+        {
+          path: "entry",
+          element: <Entry />,
+        },
+        {
+          path: "match",
+          element: <Match />,
+        },
+        {
+          path: "matchlist",
+          element: <MatchList />,
+        },
+        {
+          path: "ranking",
+          element: <Ranking />,
+        },
+        {
+          path: "result",
+          element: <Result />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "entrylist",
-        element: <EntryList />,
-      },
-      {
-        path: "entry",
-        element: <Entry />,
-      },
-      {
-        path: "match",
-        element: <Match />,
-      },
-      {
-        path: "matchlist",
-        element: <MatchList />,
-      },
-      {
-        path: "ranking",
-        element: <Ranking />,
-      },
-      {
-        path: "result",
-        element: <Result />,
-      },
-    ],
-  },
-]);
+    basename: import.meta.env.BASE_URL,
+  }
+);
