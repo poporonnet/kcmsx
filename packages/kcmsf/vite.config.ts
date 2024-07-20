55b3dc7360593c733f7374678bb922d26afe04e0
tufusa <tufusa74@gmail.com>
2024-07-20 09:51:46 +0900

    chore: `config`パッケージへのパスエイリアスを追加

--- packages/kcmsf/vite.config.ts
+++ packages/kcmsf/vite.config.ts
@@ -1,7 +1,11 @@
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: { config: path.resolve(__dirname, "../config") },
+  },
 });
