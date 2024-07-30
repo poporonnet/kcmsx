9dd6c693bcc0a8d2fe7467a2f0c34062d206a3a4
Suzune2741 <kannra1174@gmail.com>
2024-07-30 21:28:36 +0900

    fix:routerのpathを間違えていたので修正

--- packages/kcmsf/src/routes/router.tsx
+++ packages/kcmsf/src/routes/router.tsx
@@ -28,7 +28,7 @@ export const router = createBrowserRouter(
           element: <Entry />,
         },
         {
-          path: "entrybulk",
+          path: "entry/bulk",
           element: <EntryBulk />,
         },
         {
