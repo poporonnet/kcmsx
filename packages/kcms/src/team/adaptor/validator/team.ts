f670f2b46853410e2c76e4567e758620d21c0c06
tufusa <me@tufusa.net>
2024-09-07 23:38:04 +0900

    feat: `post /team`のスキーマとルートを定義 (#284)
    * chore: `team/`ディレクトリに移動
    
    * chore: `CommonErrorSchema`を上に移動
    
    * chore: `TeamSchema`の誤りを修正 `member`→`members`
    
    * chore: `TeamsResponseSchema`を`GetTeamsResponseSchema`にリネーム
    
    * chore: `post /team`のスキーマ
    
    * feat: `post /team`のルートを作成
    
    * docs: `post /team`のリクエストとレスポンスを配列に修正

--- packages/kcms/src/team/adaptor/validator/team.ts
+++ packages/kcms/src/team/adaptor/validator/team.ts
@@ -1,11 +1,15 @@
 import { z } from '@hono/zod-openapi';
 import { config, pick } from 'config';
 
+export const CommonErrorSchema = z.object({
+  description: z.string().openapi({ example: '存在しないカテゴリです' }),
+});
+
 const TeamSchema = z.object({
   id: z.string().openapi({ example: '1392387' }),
   name: z.string().openapi({ example: 'かに１' }),
   entryCode: z.string().openapi({ example: '1' }),
-  member: z
+  members: z
     .array(z.string())
     .max(2)
     .nonempty()
@@ -16,12 +20,18 @@ const TeamSchema = z.object({
   isEntered: z.boolean().openapi({ example: true }),
 });
 
-export const TeamsResponseSchema = z
+export const GetTeamsResponseSchema = z
   .object({
     teams: z.array(TeamSchema),
   })
   .openapi('Teams');
 
-export const CommonErrorSchema = z.object({
-  description: z.string().openapi({ example: '存在しないカテゴリです' }),
-});
+export const PostTeamsRequestSchema = z.array(
+  TeamSchema.omit({
+    id: true,
+    entryCode: true,
+    isEntered: true,
+  })
+);
+
+export const PostTeamsResponseSchema = z.array(TeamSchema).openapi('Teams');
