4baf8a6307f88f5a3dce8c89b8c5110731017e38
Tatsuto YAMAMOTO <me@laminne33569.net>
2024-09-07 23:57:54 +0900

    feat: JSONRepositoryを削除、repositoryに情報更新メソッドを追加

--- packages/kcms/src/team/adaptor/dummyRepository.ts
+++ packages/kcms/src/team/adaptor/dummyRepository.ts
@@ -1,44 +1,59 @@
 import { TeamRepository } from '../models/repository.js';
 import { Option, Result } from '@mikuroxina/mini-fn';
-import { Team } from '../models/team.js';
+import { Team, TeamID } from '../models/team.js';
 
 export class DummyRepository implements TeamRepository {
-  private data: Array<Team>;
-  constructor(data?: Array<Team>) {
-    this.data = data ?? [];
+  private data: Map<TeamID, Team>;
+
+  constructor(data: Team[] = []) {
+    this.data = new Map<TeamID, Team>(data.map((v) => [v.getId(), v]));
   }
 
   async create(entry: Team): Promise<Result.Result<Error, Team>> {
-    this.data.push(entry);
+    const res = this.data.set(entry.getId(), entry);
+    if (!res) {
+      return Result.err(new Error('Team not found'));
+    }
+
     return Result.ok(entry);
   }
 
   async findByTeamName(name: string): Promise<Option.Option<Team>> {
-    const entry = this.data.find((e) => e.getTeamName() === name);
-    if (entry === undefined) {
+    const entry = [...this.data].map((v) => v[1]).find((e) => e.getTeamName() === name);
+    if (!entry) {
       return Option.none();
     }
     return Option.some(entry);
   }
 
   async findByID(id: string): Promise<Option.Option<Team>> {
-    const entry = this.data.find((e) => e.getId() === id);
+    const entry = [...this.data].map((v) => v[1]).find((e) => e.getId() === id);
     if (entry === undefined) {
       return Option.none();
     }
     return Option.some(entry);
   }
 
-  async findAll(): Promise<Result.Result<Error, Array<Team>>> {
-    return Result.ok(this.data);
+  async findAll(): Promise<Result.Result<Error, Team[]>> {
+    return Result.ok([...this.data].map((v) => v[1]));
   }
 
-  async delete(id: string): Promise<Option.Option<Error>> {
-    this.data = this.data.filter((e) => e.getId() !== id);
+  async delete(id: TeamID): Promise<Option.Option<Error>> {
+    this.data.delete(id);
     return Option.none();
   }
 
-  reset() {
-    this.data = [];
+  async update(team: Team): Promise<Result.Result<Error, Team>> {
+    const res = this.data.get(team.getId());
+    if (!res) {
+      return Result.err(new Error('Team not found'));
+    }
+
+    this.data.set(team.getId(), team);
+    return Result.ok(team);
+  }
+
+  reset(data: Team[] = []) {
+    this.data = new Map<TeamID, Team>(data.map((v) => [v.getId(), v]));
   }
 }
