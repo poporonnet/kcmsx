9a651dc9acaac3f649913a98802e8e34efab60f8
tufusa <tufusa74@gmail.com>
2024-09-08 13:07:53 +0900

    feat: 試合種別設定に`course`項目を追加

--- packages/config/src/types/matchConfig.ts
+++ packages/config/src/types/matchConfig.ts
@@ -1,9 +1,17 @@
+import { DepartmentConfig } from "./departmentConfig";
 import { UniqueRecords } from "./uniqueCollection";
 
 /**
  * @description 1つの試合種別設定の型
  */
-export type MatchConfig = DerivedMatchConfig<string, string, number>;
+export type MatchConfig = DerivedMatchConfig<
+  string,
+  string,
+  number,
+  string[],
+  DepartmentConfig<string[]>[],
+  DerivedCourseConfig<string[], DepartmentConfig<string[]>[]>
+>;
 
 /**
  * @description 1つの試合種別設定の, リテラル型から導出される型
@@ -12,11 +20,23 @@ export type DerivedMatchConfig<
   Type extends string,
   Name extends string,
   LimitSeconds extends number,
+  RobotTypes extends string[],
+  Departments extends DepartmentConfig<RobotTypes>[],
+  Course extends DerivedCourseConfig<RobotTypes, Departments>,
 > = {
   type: Type;
   name: Name;
   limitSeconds: LimitSeconds;
+  course: Course;
 };
+
+/**
+ * @description 1つのコース設定の, リテラル型から導出される型
+ */
+export type DerivedCourseConfig<
+  RobotTypes extends string[],
+  Departments extends DepartmentConfig<RobotTypes>[],
+> = Partial<Record<Departments[number]["type"], number>>;
 
 /**
  * @description {@link MatchConfig}の配列から導出される試合種別設定のオブジェクト
@@ -31,7 +51,20 @@ export type DerivedMatch<Matches extends MatchConfig[]> = {
  */
 export type ValidMatchConfigs<Matches extends MatchConfig[]> =
   UniqueRecords<Matches, "type"> extends infer U
-    ? Matches extends U
-      ? Matches
-      : U
+    ? ValidCourseConfigs<Matches> extends infer C
+      ? Matches extends U
+        ? Matches extends C
+          ? Matches
+          : Matches & C // `course`が空でも補完を効かせるため
+        : Matches & U // `course`が空でも補完を効かせるため
+      : never
     : never;
+
+/**
+ * @description {@link Matches}の`course`が有効か判定する型
+ * {@link MatchConfig}の`course`オブジェクトが空ならコンパイルに失敗する
+ */
+type ValidCourseConfigs<Matches extends MatchConfig[]> =
+  {} extends Matches[number]["course"]
+    ? "Empty `course` is not allowed"
+    : Matches;
