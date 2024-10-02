747493db76ba603e2004223c3323ea2acd5c3919
Suzune2741 <kannra1174@gmail.com>
2024-10-02 13:34:53 +0900

    [wip]feat:ゴールタイムを追加

--- packages/kcmsf/src/pages/matchResult.tsx
+++ packages/kcmsf/src/pages/matchResult.tsx
@@ -1,29 +1,17 @@
 import { Flex, Paper, Text, Title } from "@mantine/core";
 const getResult = () => {
   const result = {
-    left: { teamName: "team1", points: 1, time: 0 },
-    right: { teamName: "team2", points: 3, time: 0 },
+    left: { teamName: "team1", points: 1, time: 1 },
+    right: { teamName: "team2", points: 3, time: 4 },
   };
   return result;
 };
 type MatchResult = {
   id: number;
-  mathcCode: string;
-  leftTeam: {
-    id: number;
-    teamName: string;
-  };
-  rightTeam: {
-    id: number;
-    teamName: string;
-  };
-  runResult: {
-    id: number;
-    teamID: number;
-    points: number;
-    goalTimeSeconds: number;
-    finishState: string;
-  }
+  teamid: number;
+  points: number;
+  time: number;
+  finishState: string;
 };
 export const MatchResult = () => {
   //const [matchResult, setMatchResult] = useState<MatchResult | undefined>();
@@ -33,7 +21,7 @@ export const MatchResult = () => {
   return (
     <Flex h="100%" direction="column" gap="md" align="center" justify="center">
       <Title order={1} m="1rem">
-        試合結果 
+        試合結果
       </Title>
       <Paper w="100%" withBorder>
         <Flex align="center" justify="center">
@@ -52,10 +40,21 @@ export const MatchResult = () => {
             </Text>
           </Flex>
           {rightTeamResult && (
-            <Text pr="md" size="2rem" c="red" style={{ flex: 1 }} ml = "1rem">
+            <Text pr="md" size="2rem" c="red" style={{ flex: 1 }} ml="1rem">
               {rightTeamResult.teamName}
             </Text>
           )}
+        </Flex>
+        <Flex align="center" justify="center">
+          <Flex pb="sm" gap="sm">
+            <Text size="4rem" c="blue">
+              {leftTeamResult.time}
+            </Text>
+            <Text size="4rem">-</Text>
+            <Text size="4rem" c="red">
+              {rightTeamResult.time}
+            </Text>
+          </Flex>
         </Flex>
       </Paper>
     </Flex>
