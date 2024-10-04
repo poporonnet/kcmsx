9c790fd67564bdda1a41f2da6d06c80128247580
Suzune2741 <kannra1174@gmail.com>
2024-10-04 16:50:40 +0900

    feat: matchTypeで処理を分岐させるようにした

--- packages/kcms/src/match/service/fetchRunResult.ts
+++ packages/kcms/src/match/service/fetchRunResult.ts
@@ -14,18 +14,20 @@ export class FetchRunResultService {
     matchType: MatchType,
     matchID: MainMatchID | PreMatchID
   ): Promise<Result.Result<Error, RunResult[]>> {
-    const mainMatchRes = await this.mainMatchRepository.findByID(matchID as MainMatchID);
-    if (Option.isSome(mainMatchRes)) {
+    if (matchType === 'main') {
+      const mainMatchRes = await this.mainMatchRepository.findByID(matchID as MainMatchID);
+      if (Option.isNone(mainMatchRes)) {
+        return Result.err(new Error('MainMatch not found'));
+      }
       const mainMatch = Option.unwrap(mainMatchRes);
       return Result.ok(mainMatch.getRunResults());
-    }
-    
-    const preMatchRes = await this.preMatchRepository.findByID(matchID as PreMatchID);
-    if (Option.isSome(preMatchRes)) {
+    } else {
+      const preMatchRes = await this.preMatchRepository.findByID(matchID as PreMatchID);
+      if (Option.isNone(preMatchRes)) {
+        return Result.err(new Error('PreMatch not found'));
+      }
       const preMatch = Option.unwrap(preMatchRes);
       return Result.ok(preMatch.getRunResults());
     }
-
-    return Result.err(new Error('Match not found'));
   }
 }
