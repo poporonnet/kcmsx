f3dddde98c24a1724b77a994f5303c06b90b9df0
Suzune2741 <kannra1174@gmail.com>
2024-10-20 16:40:20 +0900

    [wip]feat: 試合結果の詳細画面

--- packages/kcmsf/src/pages/matchResult.tsx
+++ packages/kcmsf/src/pages/matchResult.tsx
@@ -1,62 +1,70 @@
-import { Flex, Paper, Text, Title } from "@mantine/core";
-const getResult = () => {
-  const result = {
-    left: { teamName: "team1", points: 1, time: 1 },
-    right: { teamName: "team2", points: 3, time: 4 },
-  };
-  return result;
-};
-type MatchResult = {
-  id: number;
-  teamid: number;
-  points: number;
-  time: number;
-  finishState: string;
-};
+import { MatchType } from "config";
+import { useCallback, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { RunResult } from "../types/runResult";
+
 export const MatchResult = () => {
-  //const [matchResult, setMatchResult] = useState<MatchResult | undefined>();
-  const result = getResult();
-  const rightTeamResult = result.right;
-  const leftTeamResult = result.left;
+  const [rightTeamResult, setRightTeamResult] = useState<RunResult>();
+  const [leftTeamResult, setLeftTeamResult] = useState<RunResult>();
+  const { id, matchType } = useParams<{ id: string; matchType: MatchType }>();
+  const featchMatchResult = useCallback(async () => {
+    const res = await fetch(
+      `${import.meta.env.VITE_API_URL}/match/${matchType}/${id}`,
+      { method: "GET" }
+    );
+    if (!res.ok) return;
+    const matchDatas = await res.json();
+    console.log(matchDatas);
+    const rightTeamID = matchDatas.leftTeam.id;
+    const rightTeamResult = matchDatas.runResults.find(
+      (result: RunResult) => result.teamID === rightTeamID
+    );
+    setRightTeamResult(rightTeamResult);
+    console.log(rightTeamResult.points);
+  }, []);
+  useEffect(() => {
+    featchMatchResult();
+  }, [featchMatchResult]);
   return (
-    <Flex h="100%" direction="column" gap="md" align="center" justify="center">
-      <Title order={1} m="1rem">
-        試合結果
-      </Title>
-      <Paper w="100%" withBorder>
-        <Flex align="center" justify="center">
-          {leftTeamResult && (
-            <Text pl="md" size="2rem" c="blue" style={{ flex: 1 }} mr="1rem">
-              {leftTeamResult.teamName}
-            </Text>
-          )}
-          <Flex pb="sm" gap="sm">
-            <Text size="4rem" c="blue">
-              {leftTeamResult.points}
-            </Text>
-            <Text size="4rem">-</Text>
-            <Text size="4rem" c="red">
-              {rightTeamResult.points}
-            </Text>
-          </Flex>
-          {rightTeamResult && (
-            <Text pr="md" size="2rem" c="red" style={{ flex: 1 }} ml="1rem">
-              {rightTeamResult.teamName}
-            </Text>
-          )}
-        </Flex>
-        <Flex align="center" justify="center">
-          <Flex pb="sm" gap="sm">
-            <Text size="4rem" c="blue">
-              {leftTeamResult.time}
-            </Text>
-            <Text size="4rem">-</Text>
-            <Text size="4rem" c="red">
-              {rightTeamResult.time}
-            </Text>
-          </Flex>
-        </Flex>
-      </Paper>
-    </Flex>
+    <></>
+    //   <Flex h="100%" direction="column" gap="md" align="center" justify="center">
+    //     <Title order={1} m="1rem">
+    //       試合結果
+    //     </Title>
+    //     <Paper w="100%" withBorder>
+    //       <Flex align="center" justify="center">
+    //         {leftTeamResult && (
+    //           <Text pl="md" size="2rem" c="blue" style={{ flex: 1 }} mr="1rem">
+    //             {leftTeamResult.teamName}
+    //           </Text>
+    //         )}
+    //         <Flex pb="sm" gap="sm">
+    //           <Text size="4rem" c="blue">
+    //             {leftTeamResult.points}
+    //           </Text>
+    //           <Text size="4rem">-</Text>
+    //           <Text size="4rem" c="red">
+    //             {rightTeamResult.points}
+    //           </Text>
+    //         </Flex>
+    //         {rightTeamResult && (
+    //           <Text pr="md" size="2rem" c="red" style={{ flex: 1 }} ml="1rem">
+    //             {rightTeamResult.teamName}
+    //           </Text>
+    //         )}
+    //       </Flex>
+    //       <Flex align="center" justify="center">
+    //         <Flex pb="sm" gap="sm">
+    //           <Text size="4rem" c="blue">
+    //             {leftTeamResult.time}
+    //           </Text>
+    //           <Text size="4rem">-</Text>
+    //           <Text size="4rem" c="red">
+    //             {rightTeamResult.time}
+    //           </Text>
+    //         </Flex>
+    //       </Flex>
+    //     </Paper>
+    //   </Flex>
   );
 };
