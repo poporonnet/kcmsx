e400eb6903b37f29952068983674b487edf6b762
mentaiko <69947910+speak-mentaiko@users.noreply.github.com>
2024-10-24 22:22:49 +0900

    fix:MainMatchに走行結果を追加できるよう変更 (#573)
    * fix:追加できるよう変更
    
    * fix:テストの内容を変更
    
    * fix:テスト内容を変更
    
    * feat: 走行結果を正しく保存するように
    
    ---------
    
    Co-authored-by: tufusa <me@tufusa.net>
    Co-authored-by: tufusa <tufusa74@gmail.com>

--- packages/kcms/src/match/model/main.test.ts
+++ packages/kcms/src/match/model/main.test.ts
@@ -28,21 +28,19 @@ describe('MainMatch', () => {
   });
 
   it('走行結果は0 or 2 or 4になる', () => {
-    const args = {
-      id: '1' as MainMatchID,
-      courseIndex: 1,
-      matchIndex: 1,
-      departmentType: config.departmentTypes[0],
-      teamId1: '2' as TeamID,
-      teamId2: '3' as TeamID,
-      winnerId: '2' as TeamID,
-      runResults: [],
-    } satisfies CreateMainMatchArgs;
-
     for (let j = 1; j < 100; j++) {
-      const mainMatch = MainMatch.new(args);
+      const mainMatch = MainMatch.new({
+        id: '1' as MainMatchID,
+        courseIndex: 1,
+        matchIndex: 1,
+        departmentType: config.departmentTypes[0],
+        teamId1: '2' as TeamID,
+        teamId2: '3' as TeamID,
+        winnerId: '2' as TeamID,
+        runResults: [],
+      });
       // 2か4以外は足せない
-      if (j == 2 || j == 4) {
+      if (j == 1 || j == 2 || j == 4) {
         expect(() => {
           mainMatch.appendRunResults(
             [...Array(j)].map((_, i) => {
@@ -50,12 +48,13 @@ describe('MainMatch', () => {
                 id: String(i) as RunResultID,
                 goalTimeSeconds: i * 10,
                 points: 10 + i,
-                teamID: i % 2 == 0 ? args.teamId1 : args.teamId2,
+                teamID: i % 2 == 0 ? ('2' as TeamID) : ('3' as TeamID),
                 finishState: 'FINISHED',
               });
             })
           );
         }).not.toThrow(new Error('RunResult length must be 2 or 4'));
+        expect(mainMatch.getRunResults().length).toBe(j);
         continue;
       }
       expect(() => {
@@ -65,12 +64,59 @@ describe('MainMatch', () => {
               id: String(i) as RunResultID,
               goalTimeSeconds: i * 10,
               points: 10 + i,
-              teamID: i % 2 == 0 ? args.teamId1 : args.teamId2,
+              teamID: i % 2 == 0 ? ('2' as TeamID) : ('3' as TeamID),
               finishState: 'FINISHED',
             });
           })
         );
       }).toThrow(new Error('RunResult length must be 2 or 4'));
+      expect(mainMatch.getRunResults().length).toBe(0);
+    }
+  });
+
+  it('走行結果を追加できる', () => {
+    for (let i = 1; i <= 2; i++) {
+      const mainMatch = MainMatch.new({
+        id: '1' as MainMatchID,
+        courseIndex: 1,
+        matchIndex: 1,
+        departmentType: config.departmentTypes[0],
+        teamId1: '2' as TeamID,
+        teamId2: '3' as TeamID,
+        winnerId: '2' as TeamID,
+        runResults: [],
+      });
+      for (let j = 1; j < 8; j++) {
+        if (j === 1 || j == 2) {
+          expect(() => {
+            mainMatch.appendRunResults(
+              [...Array(i)].map((_, i) => {
+                return RunResult.new({
+                  id: String(i) as RunResultID,
+                  goalTimeSeconds: i * 10,
+                  points: 10 + i,
+                  teamID: i % 2 == 0 ? ('2' as TeamID) : ('3' as TeamID),
+                  finishState: 'FINISHED',
+                });
+              })
+            );
+          }).not.toThrow(new Error('RunResult length must be 2 or 4'));
+        } else {
+          expect(() => {
+            mainMatch.appendRunResults(
+              [...Array(i)].map((_, i) => {
+                return RunResult.new({
+                  id: String(i) as RunResultID,
+                  goalTimeSeconds: i * 10,
+                  points: 10 + i,
+                  teamID: i % 2 == 0 ? ('2' as TeamID) : ('3' as TeamID),
+                  finishState: 'FINISHED',
+                });
+              })
+            );
+          }).toThrow(new Error('RunResult length must be 2 or 4'));
+        }
+      }
     }
   });
 
