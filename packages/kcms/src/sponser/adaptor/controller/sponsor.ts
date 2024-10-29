6c4f426087c35c869e6bcafb150e54c5f17a0072
speak-mentaiko <1717miyawaki@gmail.com>
2024-10-29 22:13:14 +0900

    fix:サービスの削除

--- packages/kcms/src/sponser/adaptor/controller/sponsor.ts
+++ packages/kcms/src/sponser/adaptor/controller/sponsor.ts
@@ -1,14 +1,10 @@
 import { z } from '@hono/zod-openapi';
 import { Result } from '@mikuroxina/mini-fn';
-import { GetSponsorService } from '../../service/getsponsor';
+import { config } from 'config';
 import { GetSponsorResponseSchema } from '../validator/sponsor';
 
 export class SponsorController {
-  constructor(private readonly getSponsor: GetSponsorService) {}
-
-  getInf(): Result.Result<Error, z.infer<typeof GetSponsorResponseSchema>> {
-    const res = this.getSponsor.getInf();
-    if (Result.isErr(res)) return Result.err(res[1]);
-    return Result.ok({ sponsor: Result.unwrap(res) });
+  getAll(): Result.Result<Error, z.infer<typeof GetSponsorResponseSchema>> {
+    return Result.ok({ sponsor: config.sponsors });
   }
 }
--- packages/kcms/src/sponser/main.ts
+++ packages/kcms/src/sponser/main.ts
@@ -1,15 +1,12 @@
 import { OpenAPIHono } from '@hono/zod-openapi';
 import { Result } from '@mikuroxina/mini-fn';
+import { SponsorController } from './adaptor/controller/sponsor';
 import { GetSponsorRoute } from './routing';
-import { GetSponsorService } from './service/getsponsor';
 
 export const sponsorHandler = new OpenAPIHono();
-const getSponsorService = new GetSponsorService();
+const sponsorController = new SponsorController();
 
 sponsorHandler.openapi(GetSponsorRoute, (c) => {
-  const sponsors = getSponsorService.getInf();
-  if (Result.isErr(sponsors)) {
-    return c.json({ description: sponsors[1].message }, 400);
-  }
-  return c.json(sponsors, 200);
+  const sponsors = sponsorController.getAll();
+  return c.json(Result.unwrap(sponsors), 200);
 });
