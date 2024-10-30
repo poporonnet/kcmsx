9aa60a71b7ded5e3582c8106384527a0f9594c79
speak-mentaiko <1717miyawaki@gmail.com>
2024-10-30 23:55:35 +0900

    fix:エラーハンドリングの追加

--- packages/kcms/src/sponser/main.ts
+++ packages/kcms/src/sponser/main.ts
@@ -8,5 +8,6 @@ const sponsorController = new SponsorController();
 
 sponsorHandler.openapi(GetSponsorRoute, (c) => {
   const sponsors = sponsorController.getAll();
+  if (Result.isErr(sponsors)) return c.json({ description: sponsors[1].message }, 400);
   return c.json(Result.unwrap(sponsors), 200);
 });
