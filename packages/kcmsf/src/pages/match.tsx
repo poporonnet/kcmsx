67ceec242c668c44b6806839030afeda3c20b953
Suzune2741 <kannra1174@gmail.com>
2024-10-30 21:43:40 +0900

    feat:個別の試合結果のページを作成

--- packages/kcmsf/src/pages/match.tsx
+++ packages/kcmsf/src/pages/match.tsx
@@ -11,6 +11,7 @@ import { useJudge } from "../hooks/useJudge";
 import { useMatchInfo } from "../hooks/useMatchInfo";
 import { useMatchTimer } from "../hooks/useMatchTimer";
 import { parseSeconds } from "../utils/time";
+import { MatchResult } from "./matchResult";
 
 export const Match = () => {
   const { id, matchType } = useParams<{ id: string; matchType: MatchType }>();
@@ -33,137 +34,156 @@ export const Match = () => {
     },
     [matchJudge, forceReload]
   );
-
+  const isMatchFinished = match && match?.runResults.length >= 2 ? true : false;
   return (
-    <Flex h="100%" direction="column" gap="md" align="center" justify="center">
-      {matchInfo && (
-        <Paper w="100%" p="xs" withBorder>
-          <Flex direction="row" align="center" justify="center">
-            <Text size="2rem" c="blue" flex={1}>
-              {matchInfo?.teams.left?.teamName}
-            </Text>
-            <Flex direction="column" align="center" justify="center" c="dark">
-              {config.match[matchInfo?.matchType].name}
-              <Text size="2rem">#{match?.matchCode}</Text>
-              {match?.matchType == "main" &&
-                `${match.runResults.length == 0 ? 1 : 2}試合目`}
+    <>
+      {isMatchFinished ? (
+        <MatchResult />
+      ) : (
+        <Flex
+          h="100%"
+          direction="column"
+          gap="md"
+          align="center"
+          justify="center"
+        >
+          {matchInfo && (
+            <Paper w="100%" p="xs" withBorder>
+              <Flex direction="row" align="center" justify="center">
+                <Text size="2rem" c="blue" flex={1}>
+                  {matchInfo?.teams.left?.teamName}
+                </Text>
+                <Flex
+                  direction="column"
+                  align="center"
+                  justify="center"
+                  c="dark"
+                >
+                  {config.match[matchInfo?.matchType].name}
+                  <Text size="2rem">#{match?.matchCode}</Text>
+                  {match?.matchType == "main" &&
+                    `${match.runResults.length == 0 ? 1 : 2}試合目`}
+                </Flex>
+                <Text size="2rem" c="red" flex={1}>
+                  {matchInfo?.teams.right?.teamName}
+                </Text>
+              </Flex>
+            </Paper>
+          )}
+          <Button
+            w="100%"
+            h="auto"
+            pb="sm"
+            variant="filled"
+            color={
+              timerState == "finished" ? "pink" : isRunning ? "teal" : "gray"
+            }
+            onClick={switchTimer}
+          >
+            <Text size="5rem">{parseSeconds(totalSeconds)}</Text>
+          </Button>
+          <Paper w="100%" withBorder>
+            <Flex align="center" justify="center">
+              <Button
+                flex={1}
+                variant="transparent"
+                c="blue"
+                leftSection={<IconRotate />}
+                size="xl"
+                fw="normal"
+                onClick={() => onClickReset("left")}
+              >
+                リセット
+              </Button>
+              <Flex pb="sm" gap="sm">
+                <Text size="4rem" c="blue">
+                  {isExhibition || matchInfo?.teams.left
+                    ? matchJudge.leftTeam.point.point()
+                    : 0}
+                </Text>
+                <Text size="4rem">-</Text>
+                <Text size="4rem" c="red">
+                  {isExhibition || matchInfo?.teams.right
+                    ? matchJudge.rightTeam.point.point()
+                    : 0}
+                </Text>
+              </Flex>
+              <Button
+                flex={1}
+                variant="transparent"
+                c="red"
+                leftSection={<IconRotate />}
+                size="xl"
+                fw="normal"
+                onClick={() => onClickReset("right")}
+              >
+                リセット
+              </Button>
             </Flex>
-            <Text size="2rem" c="red" flex={1}>
-              {matchInfo?.teams.right?.teamName}
-            </Text>
-          </Flex>
-        </Paper>
-      )}
-      <Button
-        w="100%"
-        h="auto"
-        pb="sm"
-        variant="filled"
-        color={timerState == "finished" ? "pink" : isRunning ? "teal" : "gray"}
-        onClick={switchTimer}
-      >
-        <Text size="5rem">{parseSeconds(totalSeconds)}</Text>
-      </Button>
-      <Paper w="100%" withBorder>
-        <Flex align="center" justify="center">
-          <Button
-            flex={1}
-            variant="transparent"
-            c="blue"
-            leftSection={<IconRotate />}
-            size="xl"
-            fw="normal"
-            onClick={() => onClickReset("left")}
-          >
-            リセット
-          </Button>
-          <Flex pb="sm" gap="sm">
-            <Text size="4rem" c="blue">
-              {isExhibition || matchInfo?.teams.left
-                ? matchJudge.leftTeam.point.point()
-                : 0}
-            </Text>
-            <Text size="4rem">-</Text>
-            <Text size="4rem" c="red">
-              {isExhibition || matchInfo?.teams.right
-                ? matchJudge.rightTeam.point.point()
-                : 0}
-            </Text>
-          </Flex>
-          <Button
-            flex={1}
-            variant="transparent"
-            c="red"
-            leftSection={<IconRotate />}
-            size="xl"
-            fw="normal"
-            onClick={() => onClickReset("right")}
-          >
-            リセット
-          </Button>
-        </Flex>
-      </Paper>
-      <Divider w="100%" />
-      {matchJudge && (
-        <Flex direction="row" gap="2rem" align="center" justify="center">
-          <PointControls
-            color="blue"
-            team={matchJudge.leftTeam}
-            onChange={forceReload}
-            onGoal={(done) =>
-              matchJudge.goalLeftTeam(
-                done ? matchTimeSec - totalSeconds : undefined
-              )
-            }
-            disabled={!isExhibition && !matchInfo?.teams.left}
-          />
-          <Divider orientation="vertical" />
-          <PointControls
-            color="red"
-            team={matchJudge.rightTeam}
-            onChange={forceReload}
-            onGoal={(done) =>
-              matchJudge.goalRightTeam(
-                done ? matchTimeSec - totalSeconds : undefined
-              )
-            }
-            disabled={!isExhibition && !matchInfo?.teams.right}
-          />
-        </Flex>
-      )}
-      {!isExhibition && matchInfo && matchJudge && (
-        <MatchSubmit
-          matchInfo={matchInfo}
-          available={
-            timerState == "finished" ||
-            ((!matchInfo.teams.left ||
-              matchJudge.leftTeam.goalTimeSeconds != null ||
-              matchJudge.leftTeam.point.state.finish) &&
-              (!matchInfo.teams.right ||
-                matchJudge.rightTeam.goalTimeSeconds != null ||
-                matchJudge.rightTeam.point.state.finish))
-          }
-          result={{
-            left: matchInfo.teams.left && {
-              points: matchJudge.leftTeam.point.point(),
-              time: matchJudge.leftTeam.goalTimeSeconds,
-              teamName: matchInfo.teams.left.teamName,
-            },
-            right: matchInfo.teams.right && {
-              points: matchJudge.rightTeam.point.point(),
-              time: matchJudge.rightTeam.goalTimeSeconds,
-              teamName: matchInfo.teams.right.teamName,
-            },
-          }}
-          onSubmit={(isSucceeded) => {
-            if (!isSucceeded) return;
+          </Paper>
+          <Divider w="100%" />
+          {matchJudge && (
+            <Flex direction="row" gap="2rem" align="center" justify="center">
+              <PointControls
+                color="blue"
+                team={matchJudge.leftTeam}
+                onChange={forceReload}
+                onGoal={(done) =>
+                  matchJudge.goalLeftTeam(
+                    done ? matchTimeSec - totalSeconds : undefined
+                  )
+                }
+                disabled={!isExhibition && !matchInfo?.teams.left}
+              />
+              <Divider orientation="vertical" />
+              <PointControls
+                color="red"
+                team={matchJudge.rightTeam}
+                onChange={forceReload}
+                onGoal={(done) =>
+                  matchJudge.goalRightTeam(
+                    done ? matchTimeSec - totalSeconds : undefined
+                  )
+                }
+                disabled={!isExhibition && !matchInfo?.teams.right}
+              />
+            </Flex>
+          )}
+          {!isExhibition && matchInfo && matchJudge && (
+            <MatchSubmit
+              matchInfo={matchInfo}
+              available={
+                timerState == "finished" ||
+                ((!matchInfo.teams.left ||
+                  matchJudge.leftTeam.goalTimeSeconds != null ||
+                  matchJudge.leftTeam.point.state.finish) &&
+                  (!matchInfo.teams.right ||
+                    matchJudge.rightTeam.goalTimeSeconds != null ||
+                    matchJudge.rightTeam.point.state.finish))
+              }
+              result={{
+                left: matchInfo.teams.left && {
+                  points: matchJudge.leftTeam.point.point(),
+                  time: matchJudge.leftTeam.goalTimeSeconds,
+                  teamName: matchInfo.teams.left.teamName,
+                },
+                right: matchInfo.teams.right && {
+                  points: matchJudge.rightTeam.point.point(),
+                  time: matchJudge.rightTeam.goalTimeSeconds,
+                  teamName: matchInfo.teams.right.teamName,
+                },
+              }}
+              onSubmit={(isSucceeded) => {
+                if (!isSucceeded) return;
 
-            navigate(`/matchlist?match_type=${matchType}`, {
-              viewTransition: true,
-            });
-          }}
-        />
+                navigate(`/matchlist?match_type=${matchType}`, {
+                  viewTransition: true,
+                });
+              }}
+            />
+          )}
+        </Flex>
       )}
-    </Flex>
+    </>
   );
 };
--- packages/kcmsf/src/pages/matchResult.tsx
+++ packages/kcmsf/src/pages/matchResult.tsx
@@ -1,70 +1,75 @@
-import { MatchType } from "config";
-import { useCallback, useEffect, useState } from "react";
+import { Flex, Paper, Text, Title } from "@mantine/core";
+import { config, MatchType } from "config";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useMatchInfo } from "../hooks/useMatchInfo";
 import { RunResult } from "../types/runResult";
 
 export const MatchResult = () => {
   const [rightTeamResult, setRightTeamResult] = useState<RunResult>();
   const [leftTeamResult, setLeftTeamResult] = useState<RunResult>();
   const { id, matchType } = useParams<{ id: string; matchType: MatchType }>();
-  const featchMatchResult = useCallback(async () => {
-    const res = await fetch(
-      `${import.meta.env.VITE_API_URL}/match/${matchType}/${id}`,
-      { method: "GET" }
-    );
-    if (!res.ok) return;
-    const matchDatas = await res.json();
-    console.log(matchDatas);
-    const rightTeamID = matchDatas.leftTeam.id;
-    const rightTeamResult = matchDatas.runResults.find(
-      (result: RunResult) => result.teamID === rightTeamID
-    );
-    setRightTeamResult(rightTeamResult);
-    console.log(rightTeamResult.points);
-  }, []);
+  const { match, matchInfo } = useMatchInfo(id, matchType);
+  console.log(match?.runResults);
   useEffect(() => {
-    featchMatchResult();
-  }, [featchMatchResult]);
+    if (match?.runResults) {
+      setRightTeamResult(match.runResults[0]);
+      setLeftTeamResult(match.runResults[1]);
+    }
+  }, [match]);
   return (
-    <></>
-    //   <Flex h="100%" direction="column" gap="md" align="center" justify="center">
-    //     <Title order={1} m="1rem">
-    //       試合結果
-    //     </Title>
-    //     <Paper w="100%" withBorder>
-    //       <Flex align="center" justify="center">
-    //         {leftTeamResult && (
-    //           <Text pl="md" size="2rem" c="blue" style={{ flex: 1 }} mr="1rem">
-    //             {leftTeamResult.teamName}
-    //           </Text>
-    //         )}
-    //         <Flex pb="sm" gap="sm">
-    //           <Text size="4rem" c="blue">
-    //             {leftTeamResult.points}
-    //           </Text>
-    //           <Text size="4rem">-</Text>
-    //           <Text size="4rem" c="red">
-    //             {rightTeamResult.points}
-    //           </Text>
-    //         </Flex>
-    //         {rightTeamResult && (
-    //           <Text pr="md" size="2rem" c="red" style={{ flex: 1 }} ml="1rem">
-    //             {rightTeamResult.teamName}
-    //           </Text>
-    //         )}
-    //       </Flex>
-    //       <Flex align="center" justify="center">
-    //         <Flex pb="sm" gap="sm">
-    //           <Text size="4rem" c="blue">
-    //             {leftTeamResult.time}
-    //           </Text>
-    //           <Text size="4rem">-</Text>
-    //           <Text size="4rem" c="red">
-    //             {rightTeamResult.time}
-    //           </Text>
-    //         </Flex>
-    //       </Flex>
-    //     </Paper>
-    //   </Flex>
+    <Flex h="100%" direction="column" gap="md" align="center" justify="center">
+      <Title order={1}>試合結果</Title>
+      {matchInfo && (
+        <Paper w="100%" p="xs" withBorder>
+          <Flex direction="row" align="center" justify="center">
+            <Text size="2rem" c="blue" flex={1}>
+              {matchInfo?.teams.left?.teamName}
+            </Text>
+            <Flex direction="column" align="center" justify="center" c="dark">
+              {config.match[matchInfo?.matchType].name}
+              <Text size="2rem">#{match?.matchCode}</Text>
+              {match?.matchType == "main" &&
+                `${match.runResults.length == 0 ? 1 : 2}試合目`}
+            </Flex>
+            <Text size="2rem" c="red" flex={1}>
+              {matchInfo?.teams.right?.teamName}
+            </Text>
+          </Flex>
+        </Paper>
+      )}
+      <Paper w="100%" withBorder>
+        <Flex align="center" justify="center">
+          <Flex pb="sm" gap="sm">
+            <Text size="4rem" c="blue" flex={10}>
+              {rightTeamResult ? rightTeamResult.points + "点" : "結果無し"}
+            </Text>
+            <Text size="4rem" flex={1}>
+              -
+            </Text>
+            <Text size="4rem" c="red" flex={10}>
+              {leftTeamResult ? leftTeamResult.points + "点" : "結果無し"}
+            </Text>
+          </Flex>
+        </Flex>
+        <Flex align="center" justify="center">
+          <Flex pb="sm" gap="sm">
+            <Text size="4rem" c="blue" flex={10}>
+              {rightTeamResult && rightTeamResult?.goalTimeSeconds !== null
+                ? rightTeamResult.goalTimeSeconds + "秒"
+                : "フィニッシュ"}
+            </Text>
+            <Text size="4rem" flex={1}>
+              -
+            </Text>
+            <Text size="4rem" c="red" flex={10}>
+              {leftTeamResult && leftTeamResult?.goalTimeSeconds !== null
+                ? leftTeamResult.goalTimeSeconds + "秒"
+                : "フィニッシュ"}
+            </Text>
+          </Flex>
+        </Flex>
+      </Paper>
+    </Flex>
   );
 };
