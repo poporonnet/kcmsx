b13525c22082d056b3e876f8b964bd10914c4097
tufusa <tufusa74@gmail.com>
2024-11-12 12:47:30 +0900

    chore: 静的importのみ行うようにエイリアスを設定

--- packages/kcmsf/vite.config.ts
+++ packages/kcmsf/vite.config.ts
@@ -4,4 +4,10 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // 動的importを行わない
+      "@tabler/icons-react": "@tabler/icons-react/dist/esm/icons/index.mjs",
+    },
+  },
 });
