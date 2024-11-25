c5d8f6374552d793ca74eec7bb9c183b30033f0a
Miharup <74781355+kiharu3112@users.noreply.github.com>
2024-11-25 23:22:50 +0900

    chore: useCheckDataフックがちゃんと状態を持つようにした

--- packages/kcmsf/src/hooks/useCheckData.ts
+++ packages/kcmsf/src/hooks/useCheckData.ts
@@ -18,7 +18,7 @@ export type ErrorData = Record<
 
 export const useCheckData = (data: CSVRow[]) => {
   const errors: ErrorData[] = useMemo(() => {
-    return data.map(() => {
+    const errs: ErrorData[] = data.map(() => {
       return {
         teamName: undefined,
         member1: undefined,
@@ -28,49 +28,49 @@ export const useCheckData = (data: CSVRow[]) => {
         clubName: undefined,
       };
     });
+
+    data.forEach((row, i) => {
+      const { teamName, member1, member2, robotType, departmentType } = row;
+
+      // 1文字以上
+      if (teamName.length < 1) {
+        errs[i].teamName = "shortTeamName";
+      }
+      // 3文字以上
+      if (member1.length < 3) {
+        errs[i].member1 = "shortMemberName";
+      }
+
+      // 3文字以上 or ""
+      if (member2 && member2.length < 3) {
+        errs[i].member2 = "shortMemberName";
+      }
+
+      // 正しい部門
+      if (!isDepartmentType(departmentType)) {
+        errs[i].departmentType = "invalidCategory";
+      }
+
+      // 正しいロボットカテゴリー
+      if (!isRobotType(robotType)) {
+        errs[i].robotType = "invalidRobotCategory";
+      }
+    });
+
+    // teamNameの重複チェック
+    const teamNameCounts = new Map<string, number>();
+    for (const row of data) {
+      const count = teamNameCounts.get(row.teamName) ?? 0;
+      teamNameCounts.set(row.teamName, count + 1);
+    }
+
+    // 出現回数が2以上のteamNameについてエラーを設定
+    data.forEach((row, i) => {
+      if ((teamNameCounts.get(row.teamName) ?? 0) > 1) {
+        errs[i].teamName = "duplicateTeamName";
+      }
+    });
+    return errs;
   }, [data]);
-
-  data.forEach((row, i) => {
-    const { teamName, member1, member2, robotType, departmentType } = row;
-
-    // 1文字以上
-    if (teamName.length < 1) {
-      errors[i].teamName = "shortTeamName";
-    }
-    // 3文字以上
-    if (member1.length < 3) {
-      errors[i].member1 = "shortMemberName";
-    }
-
-    // 3文字以上 or ""
-    if (member2 && member2.length < 3) {
-      errors[i].member2 = "shortMemberName";
-    }
-
-    // 正しい部門
-    if (!isDepartmentType(departmentType)) {
-      errors[i].departmentType = "invalidCategory";
-    }
-
-    // 正しいロボットカテゴリー
-    if (!isRobotType(robotType)) {
-      errors[i].robotType = "invalidRobotCategory";
-    }
-  });
-
-  // teamNameの重複チェック
-  const teamNameCounts = new Map<string, number>();
-  for (const row of data) {
-    const count = teamNameCounts.get(row.teamName) ?? 0;
-    teamNameCounts.set(row.teamName, count + 1);
-  }
-
-  // 出現回数が2以上のteamNameについてエラーを設定
-  data.forEach((row, i) => {
-    if ((teamNameCounts.get(row.teamName) ?? 0) > 1) {
-      errors[i].teamName = "duplicateTeamName";
-    }
-  });
-
   return errors;
 };
