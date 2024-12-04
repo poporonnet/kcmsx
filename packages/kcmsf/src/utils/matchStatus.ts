124617927707b315816ada4ed26a49db344e98bc
speak-mentaiko <1717miyawaki@gmail.com>
2024-12-04 17:17:38 +0900

    fix: 以前のロジックを使用

--- packages/kcmsf/src/utils/matchStatus.ts
+++ packages/kcmsf/src/utils/matchStatus.ts
@@ -2,6 +2,12 @@ import { StatusButtonProps } from "../components/matchStatus";
 import { Match } from "../types/match";
 
 export const getMatchStatus = (match: Match): StatusButtonProps["status"] => {
-  if (match.matchType === "main" && match.runResults.length === 2) return "now";
-  return match.runResults.length === 0 ? "future" : "end";
+  if (match.runResults.length == 0) return "future";
+
+  const maxRunResultLength = {
+    pre:
+      match.matchType === "pre" && match?.leftTeam && match?.rightTeam ? 2 : 1,
+    main: 4,
+  }[match.matchType];
+  return match.runResults.length < maxRunResultLength ? "now" : "end";
 };
