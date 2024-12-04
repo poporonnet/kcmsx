8884697dcd8d95f2b26d18c3885a1906482c6aee
mentaiko <69947910+speak-mentaiko@users.noreply.github.com>
2024-12-05 08:56:42 +0900

    fix: 試合のステータスを正しく表示 (#765)

--- packages/kcmsf/src/pages/matchList.tsx
+++ packages/kcmsf/src/pages/matchList.tsx
@@ -26,6 +26,7 @@ import { MatchSegmentedControl } from "../components/MatchTypeSegmentedControl";
 import { useFetch } from "../hooks/useFetch";
 import { GetMatchesResponse } from "../types/api/match";
 import { Match } from "../types/match";
+import { getMatchStatus } from "../utils/matchStatus";
 
 export const MatchList = () => {
   const {
@@ -192,11 +193,8 @@ const MatchHead = ({ matchType }: { matchType: MatchType }) => (
 
 const MatchColumn = ({ match }: { match: Match }) => {
   const matchStatus: StatusButtonProps["status"] = useMemo(() => {
-    if (match.runResults.length == 0) return "future";
-
-    const maxRunResultLength = { pre: 2, main: 4 }[match.matchType];
-    return match.runResults.length < maxRunResultLength ? "now" : "end";
-  }, [match.runResults, match.matchType]);
+    return getMatchStatus(match);
+  }, [match]);
 
   return (
     <Table.Tr>
