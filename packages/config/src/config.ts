d1317352f7483764bd806fa704cddeecb32a9db1
speak-mentaiko <1717miyawaki@gmail.com>
2024-12-13 22:55:09 +0900

    chore: 正しいコンテスト名に変更

--- packages/config/src/config.ts
+++ packages/config/src/config.ts
@@ -3,7 +3,7 @@ import { createConfig } from "./utility/createConfig";
 
 export const config = createConfig(
   {
-    contestName: "第2回 Matz葉ガニロボコン",
+    contestName: "第2回 Matz葉がにロボコン",
     robots: [
       {
         type: "leg",
