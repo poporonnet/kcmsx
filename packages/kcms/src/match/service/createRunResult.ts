3bfe2a63d78e1698d1083fbf907d473bd5345808
tufusa <me@tufusa.net>
2025-06-15 22:02:40 +0900

    fix: 勝者の決定にSetMainMatchWinnerServiceを使用 (#1126)

--- packages/kcms/src/match/service/createRunResult.ts
+++ packages/kcms/src/match/service/createRunResult.ts
@@ -1,10 +1,12 @@
 import { Option, Result } from '@mikuroxina/mini-fn';
 import { MatchType } from 'config';
 import { SnowflakeIDGenerator } from '../../id/main';
+import { TeamID } from '../../team/models/team';
 import { MainMatch, MainMatchID } from '../model/main';
 import { PreMatchID } from '../model/pre';
 import { MainMatchRepository, PreMatchRepository } from '../model/repository';
 import { CreateRunResultArgs, RunResult } from '../model/runResult';
+import { SetMainMatchWinnerService } from './setMainWinner';
 
 /**
  * 試合の走行結果を複数更新するService
@@ -13,7 +15,8 @@ export class CreateRunResultService {
   constructor(
     private readonly idGenerator: SnowflakeIDGenerator,
     private readonly preMatchRepository: PreMatchRepository,
-    private readonly mainMatchRepository: MainMatchRepository
+    private readonly mainMatchRepository: MainMatchRepository,
+    private readonly setMainWinner: SetMainMatchWinnerService
   ) {}
 
   /**
@@ -62,7 +65,10 @@ export class CreateRunResultService {
       const match = Option.unwrap(matchRes);
       try {
         match.appendRunResults(runResults);
-        await this.mainMatchRepository.update(this.decideWinner(match));
+        const winnerRes = this.decideWinner(match);
+        if (Option.isSome(winnerRes)) {
+          this.setMainWinner.handle(match.getID(), Option.unwrap(winnerRes));
+        }
       } catch (e) {
         return Result.err(e as Error);
       }
@@ -70,24 +76,25 @@ export class CreateRunResultService {
     return Result.ok(runResults);
   }
 
-  decideWinner(match: MainMatch): MainMatch {
-    if (match.getRunResults().length !== 4) return match;
+  decideWinner(match: MainMatch): Option.Option<TeamID> {
+    if (match.getRunResults().length !== 4) return Option.none();
 
     // 全ての結果を見て、点数を足す
     const results = match.getRunResults();
-    const team1Results = results.filter((r) => r.getTeamID() === match.getTeamID1());
-    const team2Results = results.filter((r) => r.getTeamID() === match.getTeamID2());
+    const team1Id = match.getTeamID1()!;
+    const team2Id = match.getTeamID2()!;
+
+    const team1Results = results.filter((r) => r.getTeamID() === team1Id);
+    const team2Results = results.filter((r) => r.getTeamID() === team2Id);
 
     const team1Points = team1Results.reduce((acc, cur) => acc + cur.getPoints(), 0);
     const team2Points = team2Results.reduce((acc, cur) => acc + cur.getPoints(), 0);
 
     // 大きい方を勝者にする
     if (team1Points > team2Points) {
-      match.setWinnerID(match.getTeamID1()!);
-      return match;
+      return Option.some(team1Id);
     } else if (team2Points > team1Points) {
-      match.setWinnerID(match.getTeamID2()!);
-      return match;
+      return Option.some(team2Id);
     }
 
     // もし同じならば、ベストタイムが早い方を勝者にする
@@ -101,14 +108,12 @@ export class CreateRunResultService {
     }, Infinity);
 
     if (team1BestTime < team2BestTime) {
-      match.setWinnerID(match.getTeamID1()!);
-      return match;
+      return Option.some(team1Id);
     } else if (team2BestTime < team1BestTime) {
-      match.setWinnerID(match.getTeamID2()!);
-      return match;
+      return Option.some(team2Id);
     }
 
-    // それでも同じならば、何もしない
-    return match;
+    // それでも同じならば、勝者は自動決定できない
+    return Option.none();
   }
 }
