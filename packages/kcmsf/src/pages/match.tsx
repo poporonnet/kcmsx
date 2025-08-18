df2d47ef7b4fbd22813844b0b78f607fc7456fcc
Suzune2741 <131934933+Suzune2741@users.noreply.github.com>
2025-08-18 21:21:01 +0900

    feat: 試合表で走るサイドを明示した (#1287)
    * feat: 試合表で走るサイドを明示した
    
    * chore: sideを渡さなくてもよくした

--- packages/kcmsf/src/pages/match.tsx
+++ packages/kcmsf/src/pages/match.tsx
@@ -35,6 +35,7 @@ export const Match = () => {
 
   const {
     teams: [leftDisplayedTeam, rightDisplayedTeam],
+    displayedCourseName: [leftDisplayedCourseName, rightDisplayedCourseName],
     flip,
   } = useDisplayedTeam(matchInfo, matchJudge);
 
@@ -45,7 +46,6 @@ export const Match = () => {
     },
     [matchJudge, forceReload]
   );
-
   return (
     <Flex
       h="100%"
@@ -86,6 +86,8 @@ export const Match = () => {
                   </Button>
                 </>
               }
+              leftTeamCourseName={leftDisplayedCourseName}
+              rightTeamCourseName={rightDisplayedCourseName}
             />
           )}
           <Button
