4b8ef9988fc1bbd756ad02b1349414266ecf6256
tufusa <me@tufusa.net>
2025-10-11 23:24:49 +0900

    fix: 認証ページでヘッダとフッタが表示されていたのを修正

--- packages/kcmsf/src/routes/PrivateLayout.tsx
+++ packages/kcmsf/src/routes/PrivateLayout.tsx
@@ -4,17 +4,17 @@ import { AuthProvider } from "../components/AuthProvider.tsx";
 import { PrivateHeader } from "../components/PrivateHeader.tsx";
 
 export const PrivateLayout = () => (
-  <AppShell
-    header={{ height: 60, offset: true }}
-    footer={{ height: 30, offset: true }}
-    padding="md"
-  >
-    <PrivateHeader />
-    <AppShell.Main display="flex" style={{ flexDirection: "column" }}>
-      <AuthProvider>
+  <AuthProvider>
+    <AppShell
+      header={{ height: 60, offset: true }}
+      footer={{ height: 30, offset: true }}
+      padding="md"
+    >
+      <PrivateHeader />
+      <AppShell.Main display="flex" style={{ flexDirection: "column" }}>
         <Outlet />
-      </AuthProvider>
-    </AppShell.Main>
-    <AppShell.Footer>kcms &copy; 2023-2024 Poporon Network</AppShell.Footer>
-  </AppShell>
+      </AppShell.Main>
+      <AppShell.Footer>kcms &copy; 2023-2024 Poporon Network</AppShell.Footer>
+    </AppShell>
+  </AuthProvider>
 );
