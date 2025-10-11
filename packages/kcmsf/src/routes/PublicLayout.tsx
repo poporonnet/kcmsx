32e124890baa0d035fca343e1dd89b40fb9870a6
tufusa <me@tufusa.net>
2025-10-11 22:57:36 +0900

    chore: 表示に認証を必要としない `PublicLayout` を追加

---
+++ packages/kcmsf/src/routes/PublicLayout.tsx
@@ -0,0 +1,17 @@
+import { AppShell } from "@mantine/core";
+import { Outlet } from "react-router-dom";
+import { Header } from "../components/header.tsx";
+
+export const PublicLayout = () => (
+  <AppShell
+    header={{ height: 60, offset: true }}
+    footer={{ height: 30, offset: true }}
+    padding="md"
+  >
+    <Header />
+    <AppShell.Main display="flex" style={{ flexDirection: "column" }}>
+      <Outlet />
+    </AppShell.Main>
+    <AppShell.Footer>kcms &copy; 2023-2024 Poporon Network</AppShell.Footer>
+  </AppShell>
+);
