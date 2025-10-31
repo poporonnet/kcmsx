f911547f1214415d27c48e49fd736417f098bbb6
Tatsuto YAMAMOTO <me@laminne33569.net>
2025-11-01 01:01:46 +0900

    chore: 出力する値をシングルクオーテーションで囲うように

--- packages/kcms/scripts/generateKey.ts
+++ packages/kcms/scripts/generateKey.ts
@@ -12,5 +12,5 @@ const publicJwk = await crypto.subtle.exportKey('jwk', jwtSecret.publicKey);
 const cookieSecret = Buffer.from(crypto.getRandomValues(new Uint32Array(8))).toString('base64');
 
 console.info(
-  `KCMS_COOKIE_SECRET="${cookieSecret}"\nKCMS_AUTH_PRIVATE_JWK=${JSON.stringify(privateJwk)}\nKCMS_AUTH_PUBLIC_JWK=${JSON.stringify(publicJwk)}`
+  `KCMS_COOKIE_SECRET="${cookieSecret}"\nKCMS_AUTH_PRIVATE_JWK='${JSON.stringify(privateJwk)}'\nKCMS_AUTH_PUBLIC_JWK='${JSON.stringify(publicJwk)}'`
 );
