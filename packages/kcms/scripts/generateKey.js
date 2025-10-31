2eeae490c55bd04bf1d82aceba305b0eeaa2db5c
Tatsuto YAMAMOTO <me@laminne33569.net>
2025-11-01 00:21:53 +0900

    chore: 認証用シークレットの生成方法のドキュメントを追加

--- packages/kcms/scripts/generateKey.js
+++ packages/kcms/scripts/generateKey.js
@@ -6,15 +6,10 @@ const jwtSecret = await crypto.subtle.generateKey(
   true,
   ['sign', 'verify']
 );
-const cookieSecret = Buffer.from(crypto.getRandomValues(new Uint32Array(8))).toString('base64');
-
-/**
- *
- *
- */
-
 const privateJwk = await crypto.subtle.exportKey('jwk', jwtSecret.privateKey);
 const publicJwk = await crypto.subtle.exportKey('jwk', jwtSecret.publicKey);
+
+const cookieSecret = Buffer.from(crypto.getRandomValues(new Uint32Array(8))).toString('base64');
 
 console.info(
   `KCMS_COOKIE_SECRET="${cookieSecret}"\nKCMS_AUTH_PRIVATE_JWK=${JSON.stringify(privateJwk)}\nKCMS_AUTH_PUBLIC_JWK=${JSON.stringify(publicJwk)}`
