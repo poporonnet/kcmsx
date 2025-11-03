e534e2a7d0fdebb293ca47dd433cb392ff46bde4
tufusa <me@tufusa.net>
2025-11-03 23:43:13 +0900

    chore: 仮作成

--- packages/site/astro.config.mjs
+++ packages/site/astro.config.mjs
@@ -1,26 +1,38 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "kcmsx",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/poporonnet/kcmsx",
+        },
+        {
+          icon: "external",
+          label: "Poporon Network",
+          href: "https://poporon.org",
+        },
+      ],
+      sidebar: [
+        {
+          label: "ガイド",
+          autogenerate: { directory: "guides" },
+        },
+        {
+          label: "リファレンス",
+          autogenerate: { directory: "references" },
+        },
+      ],
+      editLink: {
+        baseUrl: "https://github.com/poporonnet/kcmsx/edit/main/packages/site",
+      },
+      lastUpdated: true,
+    }),
+  ],
 });
