bb7a73b8a6b7a73a7960fe58bfdcb0818a61c5f9
tufusa <me@tufusa.net>
2025-11-09 01:17:57 +0900

    chore: cookieMaxAgeにrecommended表記を追加

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -96,7 +96,7 @@ const main = async () => {
         { label: "30 minutes", value: 1800 },
         { label: "1 hour", value: 3600 },
         { label: "12 hours", value: 43200 },
-        { label: "1 day", value: 86400 },
+        { label: "1 day", value: 86400, hint: "recommended" },
         { label: "30 days", value: 2592000 },
       ],
       initialValue: 86400,
